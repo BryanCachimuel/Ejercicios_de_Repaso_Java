7fdbc5851fb04d652e8ae94401b7ed5b6da9c178
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-25 22:28:41 -0500

    creando la conexión hacia la base de datos

--- SistemaVentas/src/modelo/Conexion.java
+++ SistemaVentas/src/modelo/Conexion.java
@@ -1,9 +1,28 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Bryan
  */
 public class Conexion {
+    Connection con;
     
+    /*método de conexión hacia la base de datos*/
+    public Connection getConnection(){
+        
+        String url = "jdbc:mysql://localhost:3307/bd_ventas";
+        String user = "root";
+        String password = "admin1994";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            System.out.println("Error en la conexión hacia la base de datos");
+        }
+        return con;
+    }
 }
