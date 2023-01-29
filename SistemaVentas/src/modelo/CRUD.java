cabefbe7699f58e94cb6cd700da45190de5b2f2a
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-28 20:53:03 -0500

    añadiendo a los métodos los parámetros los cuales van a ser de tipo objeto y tendran sus datos almacenados en un array

--- SistemaVentas/src/modelo/CRUD.java
+++ SistemaVentas/src/modelo/CRUD.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface CRUD {
     /* aquí se declarará todos los métodos que se utilizaran en el menú de mantenimiento*/
     public List listar();
-    public int aniadir(Object o);
+    public int aniadir(Object[] o);
     public int actualizar(Object o);
     public void eliminar(int id);
 }
