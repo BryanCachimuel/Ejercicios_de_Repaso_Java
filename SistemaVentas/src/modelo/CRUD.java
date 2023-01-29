4b454ad87d3f1c6e63d1a5d2bab32fa582bb7509
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-28 20:08:09 -0500

    creando una interfaz la cual tendra todos los métodos que tendrá la clase ClienteDAO

---
+++ SistemaVentas/src/modelo/CRUD.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Bryan
+ */
+public interface CRUD {
+    /* aquí se declarará todos los métodos que se utilizaran en el menú de mantenimiento*/
+    public List listar();
+    public int aniadir(Object o);
+    public int actualizar(Object o);
+    public void eliminar(int id);
+}
