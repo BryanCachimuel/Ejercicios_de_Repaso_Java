6a93df10256bfd975dfc4fd17b6087cbb5ed4b85
BryanCachimuel <bryanloyo56@gmail.com>
2023-02-02 20:28:53 -0500

    creando la clase Vendedor en la que se encuentran los atributos de la entidad vendedor de la base de datos

---
+++ SistemaVentas/src/modelo/Vendedor.java
@@ -0,0 +1,64 @@
+package modelo;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Vendedor {
+    int id;
+    String dni;
+    String nombres;
+    String telefono;
+    String estado;
+    String user;
+    
+    public Vendedor(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
