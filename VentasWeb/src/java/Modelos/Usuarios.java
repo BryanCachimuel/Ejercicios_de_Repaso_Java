24aeea30c6653b4018c4cf8a580a6f8e88fa9694
BryanCachimuel <bryanloyo56@gmail.com>
2023-02-12 20:23:31 -0500

    creando la clase Usuarios la cual tiene todos los atributos de la entidad usuarios de la base de datos

--- VentasWeb/src/java/Modelos/Usuarios.java
+++ VentasWeb/src/java/Modelos/Usuarios.java
@@ -5,5 +5,79 @@ package Modelos;
  * @author Bryan
  */
 public class Usuarios {
+    int id;
+    int cedula;
+    String nombre;
+    String correo;
+    String  password;
+    String rol;
+    String estado;
     
+    public Usuarios(){}
+
+    public Usuarios(int id, int cedula, String nombre, String correo, String password, String rol, String estado) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
