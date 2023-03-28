21df1407965fc8005c5aedf72659617907f684cd
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-27 20:49:50 -0500

    añadiendo las dependencias para el proyecto y la conexión hacia la base de datos

---
+++ DAO_Estudiantes/src/main/java/dao/Conexion.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Conexion {
+    // creamos un constructor
+    private Conexion(){}
+    
+    // crear una variable donde se guarde el estado de la conexión hacia la base de datos
+    private static Connection conexion;
+    
+    // creamos una variable para crear una sola instancia desde esta clase al JFrame
+    private static Conexion instancia;
+    
+    //creamos las variables para conectarse a la base de datos
+    private static final String url = "jdbc:mysql://localhost:3307/estudiantes?useSSL=false";
+    private static final String username = "root";
+    private static final String password = "admin1994";
+    
+    // método para conectar hacia la base de datos
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, username, password);
+             /*JOptionPane.showMessageDialog(null, "Conexión hacia la base de datos exitosa" );*/
+             return conexion;
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error : " + e);
+        }
+        return conexion;
+    }
+    
+    // creamos el método para cerrar la conexion hacia la base de datos
+    public void cerrarConexion() throws SQLException{
+        try {
+            conexion.close();
+            /*JOptionPane.showMessageDialog(null, "Se cerro la conexión hacia la base de datos");*/
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error : " + e);
+            conexion.close();
+        }finally{
+            conexion.close();
+        }
+    }
+    
+    // Patrón singleton se lo pone static para poder ser accedido directamente desde otra clase con JFrame
+    public static Conexion getInstance(){
+        if(instancia == null){
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+}
