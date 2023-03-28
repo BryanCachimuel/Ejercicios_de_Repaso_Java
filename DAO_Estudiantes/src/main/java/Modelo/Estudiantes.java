a656a695ad8f9f8e6e7ed1260d29d951be759484
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-27 22:00:59 -0500

    creando la clase Estudiantes con los atributos de la tabla personas de la base de datos

---
+++ DAO_Estudiantes/src/main/java/Modelo/Estudiantes.java
@@ -0,0 +1,55 @@
+package Modelo;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Estudiantes {
+    private int id;
+    private String cedula;
+    private String nombres;
+    private int edad;
+    private String direccion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
