44eb59dec76b22318a281415aff876b3e94bf30a
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-29 19:45:03 -0500

    creando el ejemplo de un pool de conexiones

---
+++ DAO_Estudiantes/src/main/java/ejemplo_pool_conexiones/PoolConexiones.java
@@ -0,0 +1,14 @@
+package ejemplo_pool_conexiones;
+
+/**
+dependecia puesta en el archivo pom.xml para que funcione el pool de conexiones
+    <groupId>org.apache.commons</groupId>
+    <artifactId>commons-dbcp2</artifactId>
+    <version>2.8.0</version>
+ */
+public class PoolConexiones {
+    /* estableciendo como un ejemplo un pool de conexiones */
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+}
