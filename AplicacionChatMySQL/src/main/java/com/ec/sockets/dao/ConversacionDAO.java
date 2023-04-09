7c3438a3f33fa04ec2502df1c695070217163377
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-08 22:32:18 -0500

    creando una interfaz con dos métodos para el registro de mensajes para el clienteA y clienteB

---
+++ AplicacionChatMySQL/src/main/java/com/ec/sockets/dao/ConversacionDAO.java
@@ -0,0 +1,13 @@
+package com.ec.sockets.dao;
+
+/**
+ *
+ * @author Bryan
+ */
+public interface ConversacionDAO {
+    
+    public void registrarConversacionA(String mensaje);
+    
+    public void registrarConversacionB(String mensaje);
+    
+}
