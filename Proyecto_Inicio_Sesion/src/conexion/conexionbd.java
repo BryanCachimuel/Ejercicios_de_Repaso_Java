38a540cc9ba65f7d189877f83f1dc91765e8b0c1
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-16 21:03:37 -0500

    agregando en la conexión hacia la base de datos los métodos para cerrar la conexion hacia la base de datos y creando un método estatico getInstance

--- Proyecto_Inicio_Sesion/src/conexion/conexionbd.java
+++ Proyecto_Inicio_Sesion/src/conexion/conexionbd.java
@@ -2,13 +2,21 @@ package conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Bryan
  */
 public class conexionbd {
-    Connection cn;
+    
+    private conexionbd(){}
+    
+    private static Connection conexion;
+    
+    private static conexionbd instancia;
+    
     private String url = "jdbc:mysql://localhost:3307/inicio_registro_java?useSSL=false";
     private String username = "root";
     private String password = "admin1994";
@@ -16,11 +24,30 @@ public class conexionbd {
     public Connection conectarbd(){
         try {
              Class.forName("com.mysql.cj.jdbc.Driver");
-             cn = DriverManager.getConnection(url, username, password);
+             conexion = DriverManager.getConnection(url, username, password);
              System.out.println("Conexión Exitosa Hacia la Base de Datos");
         } catch (Exception e) {
             System.out.println("Error en la conexión hacia la base de datos: " + e);
         }
-        return cn;
+        return conexion;
     }
+    
+    public void cerrarConexion() throws SQLException{
+        try {
+            conexion.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error : " + e);
+            conexion.close();
+        }finally{
+            conexion.close();
+        }
+    }
+    
+    public static conexionbd getInstance(){
+         if(instancia == null){
+            instancia = new conexionbd();
+        }
+        return instancia;
+    }
+ 
 }
