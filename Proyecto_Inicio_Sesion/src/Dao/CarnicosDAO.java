14748f771bea37dc339cc723e7691b64cb446578
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-16 22:20:46 -0500

    creando el paquete Dao con la clase CarnicosDAO para poder realizar los métodos de registro,actualización, eliminación y busqueda

---
+++ Proyecto_Inicio_Sesion/src/Dao/CarnicosDAO.java
@@ -0,0 +1,32 @@
+package Dao;
+
+import Interface.MetodosUtil;
+import Modelo.Carnicos;
+
+/**
+ *
+ * @author Bryan
+ */
+public class CarnicosDAO implements MetodosUtil{
+
+    @Override
+    public void registrar(Carnicos carnicos) {
+        
+    }
+
+    @Override
+    public void actualizar(Carnicos carnicos) {
+        
+    }
+
+    @Override
+    public void eliminar(Carnicos carnicos) {
+        
+    }
+
+    @Override
+    public void buscar(Carnicos carnicos) {
+        
+    }
+    
+}
