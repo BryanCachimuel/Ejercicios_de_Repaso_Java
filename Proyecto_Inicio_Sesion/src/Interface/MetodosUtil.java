486b11bbae43757f08a415bc6e637276536f0868
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-16 22:17:45 -0500

    Creando mediante una interface los métodos que se van a utilizar para los carnicos

---
+++ Proyecto_Inicio_Sesion/src/Interface/MetodosUtil.java
@@ -0,0 +1,16 @@
+package Interface;
+
+import Modelo.Carnicos;
+
+/**
+ *
+ * @author Bryan
+ */
+public interface MetodosUtil {
+    
+    public void registrar(Carnicos carnicos);
+    public void actualizar(Carnicos carnicos);
+    public void eliminar(Carnicos carnicos);
+    public void buscar(Carnicos carnicos);
+    
+}
