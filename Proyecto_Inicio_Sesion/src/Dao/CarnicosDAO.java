8b5a0205f7a20582a5b65142361e854f78e57a57
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-17 20:09:20 -0500

    creando el método para registrar nuevos productos carnicos

--- Proyecto_Inicio_Sesion/src/Dao/CarnicosDAO.java
+++ Proyecto_Inicio_Sesion/src/Dao/CarnicosDAO.java
@@ -2,16 +2,40 @@ package Dao;
 
 import Interface.MetodosUtil;
 import Modelo.Carnicos;
+import conexion.conexionbd;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Bryan
  */
 public class CarnicosDAO implements MetodosUtil{
-
+    
+    conexionbd conexion = conexionbd.getInstance();
+    
     @Override
     public void registrar(Carnicos carnicos) {
-        
+        try {
+            Connection cn = conexion.conectarbd();
+            String sql = "INSERT INTO carnicos(nombre_carnico,precio_carnico_kilos,precio_carnico_libras,cantidad,procedencia,tipo_carnico) VALUES(?,?,?,?,?,?)";
+            PreparedStatement insertar = cn.prepareStatement(sql);
+            insertar.setString(1, carnicos.getCarnicos());
+            insertar.setDouble(2, carnicos.getPrecio_carnicos_kilos());
+            insertar.setDouble(3, carnicos.getPrecio_carnicos_libras());
+            insertar.setDouble(4, carnicos.getCantidad());
+            insertar.setString(5, carnicos.getProcedencia());
+            insertar.setString(6, carnicos.getTipo_carnico());
+            insertar.executeUpdate();
+            
+            conexion.cerrarConexion();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CarnicosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
