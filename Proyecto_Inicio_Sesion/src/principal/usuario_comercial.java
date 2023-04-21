b0faabb325e9e4882410d20d184ba71a9fd0806e
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-20 22:16:04 -0500

    creando un método donde se haga una consulta hacia la base de datos para obtener todos los registros realizados de la base de datos

--- Proyecto_Inicio_Sesion/src/principal/usuario_comercial.java
+++ Proyecto_Inicio_Sesion/src/principal/usuario_comercial.java
@@ -5,6 +5,13 @@
  */
 package principal;
 
+import conexion.conexionbd;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Bryan
@@ -14,8 +21,48 @@ public class usuario_comercial extends javax.swing.JFrame {
     /**
      * Creates new form usuario_comercial
      */
+    
+     conexionbd conexion = conexionbd.getInstance();
+    
     public usuario_comercial() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setTitle("Productos para el Usuario Comercial");
+        mostrarTabla("");
+    }
+    
+    private void mostrarTabla(String valor){
+        DefaultTableModel modelo = new DefaultTableModel();
+        modelo.addColumn("Nombre Carnico");
+        modelo.addColumn("Precio por Kg");
+        modelo.addColumn("Precio por Lb");
+        modelo.addColumn("Cantidad del Producto");
+        modelo.addColumn("Procedencia");
+        modelo.addColumn("Tipo de Carnico");
+        
+        tblTablaProductosCarnicos.setModel(modelo);
+        
+        Connection cn = conexion.conectarbd();
+        String sql = "SELECT * FROM carnicos WHERE CONCAT(nombre_carnico,' ',precio_carnico_kilos) LIKE '%"+valor+"%'";
+        String carnicos[] = new String[6];
+        try {
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                carnicos[0] = rs.getString("nombre_carnico");
+                carnicos[1] = rs.getString("precio_carnico_kilos");
+                carnicos[2] = rs.getString("precio_carnico_libras");
+                carnicos[3] = rs.getString("cantidad");
+                carnicos[4] = rs.getString("procedencia");
+                carnicos[5] = rs.getString("tipo_carnico");
+                
+                modelo.addRow(carnicos);
+            }
+            
+            tblTablaProductosCarnicos.setModel(modelo);
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+        }
     }
 
     /**
@@ -28,26 +75,49 @@ public class usuario_comercial extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblTablaProductosCarnicos = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel1.setFont(new java.awt.Font("sansserif", 0, 14)); // NOI18N
         jLabel1.setText("Bienvenido Usuario Comercial");
+
+        tblTablaProductosCarnicos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tblTablaProductosCarnicos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(198, 198, 198)
-                .addComponent(jLabel1)
-                .addContainerGap(203, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(194, 194, 194)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 504, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(39, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(208, 208, 208)
+                .addGap(44, 44, 44)
                 .addComponent(jLabel1)
-                .addContainerGap(271, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(53, Short.MAX_VALUE))
         );
 
         pack();
@@ -90,5 +160,7 @@ public class usuario_comercial extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblTablaProductosCarnicos;
     // End of variables declaration//GEN-END:variables
 }
