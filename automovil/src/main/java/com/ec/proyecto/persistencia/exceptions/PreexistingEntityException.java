ad3bf9b27e705f5d3244349afbe1856454a375d4
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-04 18:26:04 -0500

    agregando el paquete de persistencia de excepciones el cual fue creado automaticamente con el mapeo de la Entidad de persistencia Automovil

---
+++ automovil/src/main/java/com/ec/proyecto/persistencia/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.ec.proyecto.persistencia.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
