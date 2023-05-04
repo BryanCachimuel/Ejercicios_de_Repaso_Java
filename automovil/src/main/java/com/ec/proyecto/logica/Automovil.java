0856d3efa8911209f70af91553353a3f5c1f1431
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-03 19:32:39 -0500

    creando la clase Automovil perteneciente al paquete logica

--- automovil/src/main/java/com/ec/proyecto/logica/Automovil.java
+++ automovil/src/main/java/com/ec/proyecto/logica/Automovil.java
@@ -5,5 +5,90 @@ package com.ec.proyecto.logica;
  * @author Bryan
  */
 public class Automovil {
+    private int id;
+    private String modelo;
+    private String marca;
+    private String motor;
+    private String color;
+    private String placa;
+    private int cantidadPuertas;
+    private double valorAutomovil;
+
+    public Automovil() {}
+
+    public Automovil(int id, String modelo, String marca, String motor, String color, String placa, int cantidadPuertas, double valorAutomovil) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.motor = motor;
+        this.color = color;
+        this.placa = placa;
+        this.cantidadPuertas = cantidadPuertas;
+        this.valorAutomovil = valorAutomovil;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getCantidadPuertas() {
+        return cantidadPuertas;
+    }
+
+    public void setCantidadPuertas(int cantidadPuertas) {
+        this.cantidadPuertas = cantidadPuertas;
+    }
+
+    public double getValorAutomovil() {
+        return valorAutomovil;
+    }
+
+    public void setValorAutomovil(double valorAutomovil) {
+        this.valorAutomovil = valorAutomovil;
+    }
     
 }
