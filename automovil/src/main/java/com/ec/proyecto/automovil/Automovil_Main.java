a9a55fe7ad9a94eb13b96dc9fd9e8ad235c99174
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-03 21:02:55 -0500

    clase main que manda a ejecutar la jframe principal del ejercicio

---
+++ automovil/src/main/java/com/ec/proyecto/automovil/Automovil_Main.java
@@ -0,0 +1,17 @@
+package com.ec.proyecto.automovil;
+
+import com.ec.proyecto.vistas.Principal_Automovil;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Automovil_Main {
+    
+    public static void main(String[] args) {
+        Principal_Automovil p_automovil = new Principal_Automovil();
+        p_automovil.setVisible(true);
+        p_automovil.setLocationRelativeTo(null);
+    }
+
+}
