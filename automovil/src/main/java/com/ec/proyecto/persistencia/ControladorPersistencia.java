a6fd28b2f9350fcc2fd9c1db4301301d6b4d49b7
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-04 22:02:58 -0500

    creando el método agregarAutomovil el cual va a consumir el método create determinado en la clase AutomovilJpaController

--- automovil/src/main/java/com/ec/proyecto/persistencia/ControladorPersistencia.java
+++ automovil/src/main/java/com/ec/proyecto/persistencia/ControladorPersistencia.java
@@ -1,9 +1,17 @@
 package com.ec.proyecto.persistencia;
 
+import com.ec.proyecto.logica.Automovil;
+
 /**
  *
  * @author Bryan
  */
 public class ControladorPersistencia {
+    
+    AutomovilJpaController autoJPA = new AutomovilJpaController();
+
+    public void agregarAutomovil(Automovil auto) {
+        autoJPA.create(auto);
+    }
     
 }
