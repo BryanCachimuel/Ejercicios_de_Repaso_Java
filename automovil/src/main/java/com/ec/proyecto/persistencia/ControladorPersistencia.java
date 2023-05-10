2ec49130c95d94797c4c9e1b96c72ab4cbac9181
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-09 21:15:28 -0500

    creando el método eliminarAuto el cual a travéz de la persistencia se puede invocar un método especializado para eliminar los registros de la base de datos

--- automovil/src/main/java/com/ec/proyecto/persistencia/ControladorPersistencia.java
+++ automovil/src/main/java/com/ec/proyecto/persistencia/ControladorPersistencia.java
@@ -1,7 +1,10 @@
 package com.ec.proyecto.persistencia;
 
 import com.ec.proyecto.logica.Automovil;
+import com.ec.proyecto.persistencia.exceptions.NonexistentEntityException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,6 +20,14 @@ public class ControladorPersistencia {
 
     public List<Automovil> listarAutos() {
         return autoJPA.findAutomovilEntities();
+    }
+
+    public void eliminarAuto(int idAuto) {
+        try {
+            autoJPA.destroy(idAuto);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(ControladorPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
