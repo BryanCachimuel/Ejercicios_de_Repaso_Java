c7fa6e05cc5817ac26cf10c1b59baaf38d0517b8
BryanCachimuel <bryanloyo56@gmail.com>
2023-07-30 20:36:46 -0500

    creando el método para listar todos los productos que se encuentren en la base de datos

---
+++ InventarioMVC/src/modelo/ProductoDAO.java
@@ -0,0 +1,44 @@
+package modelo;
+
+import base_datos.ConexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Bryan
+ */
+public class ProductoDAO {
+    ConexionBD conexion = new ConexionBD();  // instancia de la conexión hacia la base de datos
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    public List listarProductos(){
+        String sql = "SELECT * FROM productos";
+        List<ProductoDTO> lista = new ArrayList<>();
+        try {
+            con = conexion.conectarBaseDatos();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+                ProductoDTO producto = new ProductoDTO();
+                /* los números hacen referencia a la columna de los atributos de la tabla productos*/
+                producto.setCodigo(rs.getInt(1));
+                producto.setNombre(rs.getString(2));
+                producto.setPrecio(rs.getDouble(3));
+                producto.setStock(rs.getInt(4));
+                lista.add(producto);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al listar los productos: " + e);
+        }
+        return lista;
+    }
+    
+}
