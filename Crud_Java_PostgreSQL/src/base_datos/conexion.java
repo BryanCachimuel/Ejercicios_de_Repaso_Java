8d74799e0dc58c09afcf3c38705836596f9647ff
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-19 20:25:03 -0500

    añadiendo la libreria con el jdbc de postgreesql y realizando la conexión hacia la base de datos

---
+++ Crud_Java_PostgreSQL/src/base_datos/conexion.java
@@ -0,0 +1,29 @@
+package base_datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bryan
+ */
+public class conexion {
+    Connection conn = null;
+    String url = "jdbc:postgresql://localhost:empresa";
+    String usuario = "postgres";
+    String clave = "admin1994";
+    
+    public Connection conectar(){
+        try {
+           Class.forName("org.postgresql.Driver");
+           conn = DriverManager.getConnection(url, usuario, clave);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error en la conexión hacia la base de datos: " + e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        return conn;
+    }
+    
+}
