d4a9f5ab4904a951b15f51943db80a65516fa991
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-19 19:59:49 -0500

    estableciendo el script para que sea visible, centrar y poner un título a la venta que fue diseñada anteriormente

--- Crud_Java_PostgreSQL/src/vistas/arranque.java
+++ Crud_Java_PostgreSQL/src/vistas/arranque.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vistas;
 
 /**
@@ -15,7 +10,10 @@ public class arranque {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        frmEmpresa empresa = new frmEmpresa();
+        empresa.setVisible(true);
+        empresa.setLocationRelativeTo(null);
+        empresa.setTitle("Empresa");
     }
     
 }
