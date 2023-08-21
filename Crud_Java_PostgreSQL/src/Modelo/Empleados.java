c2fb2f409820d0f84aa4d0e866e651f1f4ca5b31
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-20 20:56:46 -0500

    creando el modelo empleado donde se definen los atributos de la entidad empleado de la base de datos y también se crearon los getters y setters de los atributos de la clase

---
+++ Crud_Java_PostgreSQL/src/Modelo/Empleados.java
@@ -0,0 +1,55 @@
+package Modelo;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Empleados {
+    private static int idempleado;
+    private static String nombre;
+    private static String apellido;
+    private static String area;
+    private static String puesto;
+
+    public int getIdempleado() {
+        return idempleado;
+    }
+
+    public void setIdempleado(int idempleado) {
+        this.idempleado = idempleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    
+    
+}
