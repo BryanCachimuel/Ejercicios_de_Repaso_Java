6f4a8918e3425c27d0d17c4546d25e72dc6a0d4f
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-24 20:43:55 -0500

    añadiendo la seguridad en la conexión hacia la base de datos mediante el mejoramiento del script del class.forName

--- Crud_personas/src/modelo/Conexion.java
+++ Crud_personas/src/modelo/Conexion.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -18,9 +19,10 @@ public class Conexion {
         String pass = "admin1994";
         
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error en la conexión hacia la base de datos: "+e);
         }
         return con;
     }
