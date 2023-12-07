3f18c13eb063e9f9cddcf4d4c67e08b5b1b7cb7c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-06 22:09:03 -0500

    agregando los atributos para los roles y sus respectivos métodos get y set y su constructor

--- crud_java_mvc/src/Modelos/Roles.java
+++ crud_java_mvc/src/Modelos/Roles.java
@@ -5,4 +5,25 @@ package Modelos;
  */
 public class Roles {
     
+    private int Id;
+    private String Nombre;
+
+    public Roles() {}
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
 }
