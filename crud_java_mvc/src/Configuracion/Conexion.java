f7dc81b8c8828110010962765d2d6ebb29df31e6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-06 22:30:48 -0500

    realizando la conexión hacia la base de datos

--- crud_java_mvc/src/Configuracion/Conexion.java
+++ crud_java_mvc/src/Configuracion/Conexion.java
@@ -1,9 +1,28 @@
 package Configuracion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Bryan
  */
 public class Conexion {
     
+    private Connection cnn;
+    private String cadenaConexion, usuarioDB, claveDB;
+    
+    public Conexion(String cadenaConexion, String usuarioDB, String claveDB){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            this.cadenaConexion = cadenaConexion;
+            this.usuarioDB = usuarioDB;
+            this.claveDB = claveDB;
+            cnn = DriverManager.getConnection(this.cadenaConexion,this.usuarioDB,this.claveDB);
+            System.out.println("Conexión hacia la BDD exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error en la conexión hacia la base de datos" + e);
+        }
+    }
 }
