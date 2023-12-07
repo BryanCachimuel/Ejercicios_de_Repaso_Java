ebfa2a902220564f76b612ebf9b3ecd3b618751f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-06 22:07:51 -0500

    agregando los atributos para datos personales y a los cuales se les agregan sus respectivos métodos get y set

--- crud_java_mvc/src/Modelos/DatosPersonales.java
+++ crud_java_mvc/src/Modelos/DatosPersonales.java
@@ -1,8 +1,73 @@
 package Modelos;
+
+import java.util.Date;
+
 /**
  *
  * @author Bryan
  */
 public class DatosPersonales {
     
+    private String Nombre, Apellido, Correo, Pais, Profesion;
+    private int Rol_id;
+    private Date Fecha_nacimiento;
+
+    public DatosPersonales() {}
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+
+    public String getProfesion() {
+        return Profesion;
+    }
+
+    public void setProfesion(String Profesion) {
+        this.Profesion = Profesion;
+    }
+
+    public int getRol_id() {
+        return Rol_id;
+    }
+
+    public void setRol_id(int Rol_id) {
+        this.Rol_id = Rol_id;
+    }
+
+    public Date getFecha_nacimiento() {
+        return Fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date Fecha_nacimiento) {
+        this.Fecha_nacimiento = Fecha_nacimiento;
+    }
+
 }
