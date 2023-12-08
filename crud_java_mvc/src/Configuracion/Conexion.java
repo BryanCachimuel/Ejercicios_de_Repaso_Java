86dfe18217c3469a5fff7a9ab369e2a4c9df16ca
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-07 20:53:45 -0500

    creando cuatro métodos los cuales sirven para realizar la conexión hacia la base de datos, para testear la conexión, para abrir una conexión y para cerrar la conexión hacia la base de datos

--- crud_java_mvc/src/Configuracion/Conexion.java
+++ crud_java_mvc/src/Configuracion/Conexion.java
@@ -3,6 +3,8 @@ package Configuracion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,14 +17,39 @@ public class Conexion {
     
     public Conexion(String cadenaConexion, String usuarioDB, String claveDB){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             this.cadenaConexion = cadenaConexion;
             this.usuarioDB = usuarioDB;
             this.claveDB = claveDB;
             cnn = DriverManager.getConnection(this.cadenaConexion,this.usuarioDB,this.claveDB);
             System.out.println("Conexión hacia la BDD exitosa");
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error en la conexión hacia la base de datos" + e);
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    public boolean testearConexion(){
+        try {
+            return !cnn.isClosed();
+        } catch (SQLException e) { 
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+    
+    public void abrirConexion(){
+        try {
+            cnn = DriverManager.getConnection(cadenaConexion,"root","");
+        } catch (SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    public void cerrarConexion(){
+        try {
+            cnn.close();
+        } catch (SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 }
