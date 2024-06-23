8cf4bacebca5a7e604b5361326836ffec7c5feb4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-22 20:45:53 -0500

    Creando los métodos de conversión de Bolivianos a Euros y de Euros a Bolivianos

--- Ejercicios_Basicos/src/Conversor_Monedas/Servicio.java
+++ Ejercicios_Basicos/src/Conversor_Monedas/Servicio.java
@@ -5,7 +5,7 @@ public class Servicio {
     public double BolivianoDolar(double moneda){
         double cambio = 0;
         try {
-            cambio = moneda/6.92;
+            cambio = moneda / 6.92;
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
@@ -16,6 +16,26 @@ public class Servicio {
         double cambio = 0;
         try {
             cambio = moneda * 6.92;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return cambio;
+    }
+    
+    public double BolivianoEuro(double moneda){
+        double cambio = 0;
+        try {
+            cambio = moneda / 7.01;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return cambio;
+    }
+    
+    public double EuroBoliviano(double moneda){
+        double cambio = 0;
+        try {
+            cambio = moneda * 7.01;
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
