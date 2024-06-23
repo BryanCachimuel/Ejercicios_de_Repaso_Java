d191e315607c66148466fd55770e6594e564c6ea
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-22 20:36:59 -0500

    Creando el método para realizar el cambio de Bolivar a Dolar

---
+++ Ejercicios_Basicos/src/Conversor_Monedas/Servicio.java
@@ -0,0 +1,10 @@
+package Conversor_Monedas;
+
+public class Servicio {
+    
+    public double BolivianoDolar(double moneda){
+        double cambio = moneda/6.92;
+        return cambio;
+    }
+    
+}
