72828e5bbff2f93440fc2ad880b66f47598a3700
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-22 20:42:09 -0500

    Creando el método para hacer el cambio de Dolar a Boliviano

--- Ejercicios_Basicos/src/Conversor_Monedas/Servicio.java
+++ Ejercicios_Basicos/src/Conversor_Monedas/Servicio.java
@@ -3,7 +3,22 @@ package Conversor_Monedas;
 public class Servicio {
     
     public double BolivianoDolar(double moneda){
-        double cambio = moneda/6.92;
+        double cambio = 0;
+        try {
+            cambio = moneda/6.92;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return cambio;
+    }
+    
+    public double DolarBoliviano(double moneda){
+        double cambio = 0;
+        try {
+            cambio = moneda * 6.92;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
         return cambio;
     }
     
