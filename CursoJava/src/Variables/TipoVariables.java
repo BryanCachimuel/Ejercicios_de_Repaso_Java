6c7e6208c935c13df0dcfb7d34bb6dfef80f55d9
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-26 23:03:45 -0500

    Creando el proyecto de curso de java

---
+++ CursoJava/src/Variables/TipoVariables.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class TipoVariables {
+
+    public static void main(String[] args) {
+        int miVariableEntera = 29;
+        System.out.println(miVariableEntera);
+        
+        /* Modificando el valor de la variablle */
+        miVariableEntera = 25;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos B.L";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+    }
+    
+}
