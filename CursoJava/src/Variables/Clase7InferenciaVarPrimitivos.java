a06365932d465c49a71d86f0fb6eaf22c952a905
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-29 22:20:11 -0500

    Creando la clase para observar la inferencia entre los tipos var y primmitivos

---
+++ CursoJava/src/Variables/Clase7InferenciaVarPrimitivos.java
@@ -0,0 +1,18 @@
+package Variables;
+/*
+ Inferencia de Tipos con Var y Tipos Primitivos
+ */
+public class Clase7InferenciaVarPrimitivos {
+
+    public static void main(String[] args) {
+        var numeroEntero = 10;
+        System.out.println("numeroEntero: " + numeroEntero);
+        
+        double numeroDecimal = 25.0;
+        System.out.println("numeroDecimal: " + numeroDecimal);
+        
+        var numeroFloat = 69.7F;
+        System.out.println("numeroFloat: " + numeroFloat);
+    }
+    
+}
