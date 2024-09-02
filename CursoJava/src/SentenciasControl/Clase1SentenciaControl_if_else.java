160d857ef0648c09fed93940ea8615b64677ae1c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-01 22:38:19 -0500

    Probando el funcionamiento de las sentencias if, else con un ejercicio básico

---
+++ CursoJava/src/SentenciasControl/Clase1SentenciaControl_if_else.java
@@ -0,0 +1,19 @@
+package SentenciasControl;
+
+/*
+    Sentencia de control if lese
+ */
+public class Clase1SentenciaControl_if_else {
+
+    public static void main(String[] args) {
+        int numero = 15;
+        int numero2 = 5;
+        
+        if(numero % numero2 == 0){
+            System.out.println("Condición verdader");
+        }else{
+            System.out.println("Condición falsa");
+        }
+    }
+    
+}
