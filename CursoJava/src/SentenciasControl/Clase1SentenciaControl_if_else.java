e8d614b6a4037ef40c4184c68456062330548603
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-01 22:50:07 -0500

    Mediante un ejercicio se aplico el procedimiento del if, else if, else

--- CursoJava/src/SentenciasControl/Clase1SentenciaControl_if_else.java
+++ CursoJava/src/SentenciasControl/Clase1SentenciaControl_if_else.java
@@ -6,14 +6,35 @@ package SentenciasControl;
 public class Clase1SentenciaControl_if_else {
 
     public static void main(String[] args) {
-        int numero = 15;
+        /*int numero = 15;
         int numero2 = 5;
         
         if(numero % numero2 == 0){
             System.out.println("Condición verdader");
         }else{
             System.out.println("Condición falsa");
+        }*/
+        
+        int numeroDeterminado = 2;
+        var numeroTexto = "Número Desconocido";
+        
+        if(numeroDeterminado == 1){
+            numeroTexto = "Número Uno";
         }
+        else if(numeroDeterminado == 2){
+            numeroTexto = "Número Dos";
+        }
+        else if(numeroDeterminado == 3){
+            numeroTexto = "Número Tres";
+        }
+        else if(numeroDeterminado == 4){
+            numeroTexto = "Número Cuatro";
+        }
+        else{
+            numeroTexto = "Número no Encontrado";
+        }
+        
+        System.out.println("Número Texto: " + numeroTexto);
     }
     
 }
