2eb9a5a39ef15c546f85e2650800df9fa0c9874c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-02 21:16:03 -0500

    Ejercicio de procedimiento del ciclo for

---
+++ CursoJava/src/Ciclos/Clase3CicloFor.java
@@ -0,0 +1,15 @@
+package Ciclos;
+
+/*
+    Ciclo for
+ */
+public class Clase3CicloFor {
+
+    public static void main(String[] args) {
+        
+        for(var contador = 0; contador < 3; contador++){
+            System.out.println("contador: " + contador);
+        }
+    }
+    
+}
