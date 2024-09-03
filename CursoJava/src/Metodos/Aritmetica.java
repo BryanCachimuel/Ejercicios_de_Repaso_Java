d28d0d04623453cfe34a9595d0eb7596e5f3c453
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-02 22:18:13 -0500

    Procedimiento para la creación de métodos dentro de una clase

---
+++ CursoJava/src/Metodos/Aritmetica.java
@@ -0,0 +1,16 @@
+package Metodos;
+
+/*
+    Clase Métodos
+ */
+public class Aritmetica {
+    // atributos de la clase
+    int a;
+    int b;
+    
+    // método de clase
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("Resultado: " + resultado);
+    }
+}
