41fcd14610b58190306d7bff4f9d2baafab2a21a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-02 21:34:04 -0500

    Realizando el procedimiento de la creación de una clase Persona con sus atributos y método de clase

---
+++ CursoJava/src/Clases/Persona.java
@@ -0,0 +1,16 @@
+package Clases;
+/*
+    Clase 1: Persona
+*/
+public class Persona {
+    
+    // atributos de la clase
+    String nombre;
+    String apellido;
+
+    /* métodos de clase */
+    public void desplegarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
