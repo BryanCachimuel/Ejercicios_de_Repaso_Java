d3546db7d90e449578db8a704a880e326c80ac8d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-02 21:14:56 -0500

    Ejercicio de repaso del procedimiento del ciclo do while

---
+++ CursoJava/src/Ciclos/Clase2CicloDoWhile.java
@@ -0,0 +1,17 @@
+package Ciclos;
+
+/*
+    Ciclo Do While
+ */
+public class Clase2CicloDoWhile {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        do{
+          System.out.println("contador: " + contador);
+          contador++;
+        }while(contador < 3);
+    }
+    
+}
