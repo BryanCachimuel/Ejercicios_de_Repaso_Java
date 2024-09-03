a3fb1c4b292b1b1f64140015bd27b4b6157e8dce
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-02 21:13:43 -0500

    Ejercicio del procedimiento del uso del ciclo while

---
+++ CursoJava/src/Ciclos/Clase1CicloWhile.java
@@ -0,0 +1,17 @@
+package Ciclos;
+
+/*
+    Ciclo While
+ */
+public class Clase1CicloWhile {
+
+    public static void main(String[] args) {
+        var contador = 0;
+        
+        while(contador < 3){
+            System.out.println("contador: " + contador);
+            contador++;
+        }
+    }
+    
+}
