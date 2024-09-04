f3fc74963d8e0f36d1c8369fb8dba08c070c963c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-03 21:00:44 -0500

    Realizando pruebas de funcionamiento de los métodos definidos en la clase aritmetica

--- CursoJava/src/Metodos/PruebaAritmetica.java
+++ CursoJava/src/Metodos/PruebaAritmetica.java
@@ -15,6 +15,16 @@ public class PruebaAritmetica {
         /* para mandar a imprimir un método de tipo int se debe declarar una variable del mismo tipo del método */
         int resultado = aritmetica.sumarConRetorno();
         System.out.println("Resultado con método de retorno: " + resultado);
+        
+        Aritmetica aritmetica2 = new Aritmetica();
+        int v1 = aritmetica2.a = 15;
+        int v2 = aritmetica2.b = 25;
+        /* cuando se este en debug y se quiera entrar en el proceso del método se debe dar clik en la opcíon f7 (signo que describa f7) */
+        int resultado2 = aritmetica2.sumarConArgumentos(v1, v2);
+        System.out.println("Resultado con método con argumentos: " + resultado2);
+        
+        int resultado3 = aritmetica2.sumarConsumiendo(v1, v2);
+        System.out.println("Resultado con método que consume otro método: " + resultado3);
     }
     
 }
