3579333b2fca9bbfcfe24f57b33f500071f51d44
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-04 22:07:04 -0500

    Uso de la palabra reservada this para acceder a métodos y atrbutos de la clase

--- CursoJava/src/UsoPalabraThis/Clase1PalabraThis.java
+++ CursoJava/src/UsoPalabraThis/Clase1PalabraThis.java
@@ -19,7 +19,24 @@ class Persona {
     String apellido;
     
     Persona(String nombre, String apellido){
+        /*super(); llamada al constructor de la clase padre (object) */
         this.nombre = nombre;
         this.apellido = apellido;
+        System.out.println("Objeto persona usando this: " + this);
+        // forma de realizar la impresión sin la necesidad de instanciar un objeto
+        new Imprimir().imprimir(this);
+    }
+}
+
+class Imprimir {
+    
+    // constructor
+    public Imprimir(){
+        super(); // el constructor de la clase object(padre) para reservar memoria 
+    }
+    
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde Imprimir: " + persona);
+        System.out.println("Impresión del objeto actual(this): " + this);
     }
 }
