37d21af37780139984b0631029f03208f1c35d8c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-05 21:36:31 -0500

    Creando la clase persona con sus respectivos atributos, constructor, metodos get y set y su método toString

---
+++ CursoJava/src/ContextoEstatico/Persona.java
@@ -0,0 +1,48 @@
+package ContextoEstatico;
+
+/*
+    Contexto Estático (uso de la palabra static)
+ */
+public class Persona {
+
+    private int idPersona;
+    private String nombre;
+    private static int contadorPersonas;
+    
+    // constructor
+    public Persona(String nombre){
+        this.nombre = nombre;
+        // Incrementar el contador por cada objeto nuevo y aumentando el nuevo valor a la variable idPersona
+       this.idPersona = Persona.contadorPersonas++;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public static int getContadorPersonas() {
+        return contadorPersonas;
+    }
+
+    public static void setContadorPersonas(int aContadorPersonas) {
+        contadorPersonas = aContadorPersonas;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
+   
+}
