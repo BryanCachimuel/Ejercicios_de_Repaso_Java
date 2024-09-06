579f5bc0cad06080e01d5b16375d709104b1a5dc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-05 22:04:45 -0500

    Códificando el método static imprimir para comprobar el duncionamiento del consumo de métodos stactic desde otro método de tipo static

--- CursoJava/src/ContextoEstatico/PruebaPersona.java
+++ CursoJava/src/ContextoEstatico/PruebaPersona.java
@@ -4,6 +4,7 @@ public class PruebaPersona {
     
     public static void main(String[] args) {
         
+        /*
         Persona persona = new Persona("Mónica");
         System.out.println("persona: " + persona.toString());
         
@@ -12,7 +13,20 @@ public class PruebaPersona {
         
         Persona persona2 = new Persona("Silvana");
         System.out.println("persona2: " + persona2.toString());
+        */
+        Persona persona = new Persona("Mónica",23);
+      
+        Persona persona1 = new Persona("Vanessa",26);
         
+        Persona persona2 = new Persona("Silvana",30);
+        
+        imprimir(persona);
+        imprimir(persona1);
+        imprimir(persona2);
+    }
+    
+    public static void imprimir(Persona persona){
+        System.out.println("persona = " + persona);
     }
  
 }
