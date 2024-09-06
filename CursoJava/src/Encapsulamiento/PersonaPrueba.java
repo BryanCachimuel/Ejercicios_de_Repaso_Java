cac51029deb6622b58eeeb75f39f103c6242c4a0
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-05 21:18:28 -0500

    Comprobando el funcionamiento del método toString

--- CursoJava/src/Encapsulamiento/PersonaPrueba.java
+++ CursoJava/src/Encapsulamiento/PersonaPrueba.java
@@ -5,12 +5,17 @@ public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona = new Persona("Lennin", 600.00, false);
         System.out.println("Nombre: " + persona.getNombre());
+        // llamando al método toString de forma automática
+        System.out.println("persona: " + persona);
         
         // para modificar los datos de los atributos
         persona.setNombre("Bryan");
         System.out.println("Persona Nombre: " + persona.getNombre());
         System.out.println("Persona Sueldo: " + persona.getSueldo());
         System.out.println("Persona Eliminadi: " + persona.isEliminado());
+        
+        String impresion = persona.toString();
+        System.out.println("persona: " +impresion);
     }
     
 }
