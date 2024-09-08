c6ebbe5405ad87fc006dafcc10b43b2c97113272
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-07 22:19:22 -0500

    Creando otro método sumar en este caso de tipo void para comprobar la sobrecarga de métodos

--- CursoJava/src/SobrecargaMetodos/Operaciones.java
+++ CursoJava/src/SobrecargaMetodos/Operaciones.java
@@ -6,10 +6,18 @@ package SobrecargaMetodos;
 public class Operaciones {
     
     public static int sumar(int a, int b){
+        System.out.println("sumar(int a, int b)");
         return a + b;
     }
     
     public static double sumar(double a, double b){
+        System.out.println("sumar(double a, double b)");
         return a + b;
+    }
+    
+    public static void sumar(int a, int b, int c){
+        System.out.println("sumar(int a, int b, int c)");
+        int d = (a + c) + b;
+        System.out.println("Resultado suma3 = " + d);
     }
 }
