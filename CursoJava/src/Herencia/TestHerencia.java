4ca80757f49ec6c52490b24fd7ee16ca2b9822fc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-07 20:58:35 -0500

    Creando un clase de prueba para verificar el funcionamiento del ejercicio en lo que respecta a herencia

---
+++ CursoJava/src/Herencia/TestHerencia.java
@@ -0,0 +1,10 @@
+package Herencia;
+
+public class TestHerencia {
+    
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Nelson", 600.00);
+        System.out.println("Empleado: " + empleado);
+    }
+    
+}
