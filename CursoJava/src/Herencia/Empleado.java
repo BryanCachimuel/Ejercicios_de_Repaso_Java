70a6c90c82c206fdc35756d22c806c241cee6c79
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-07 21:58:04 -0500

    Poniendo a prueba la sobrecarga de constructores y el consumo entre los mismos

--- CursoJava/src/Herencia/Empleado.java
+++ CursoJava/src/Herencia/Empleado.java
@@ -6,9 +6,16 @@ public class Empleado extends Persona{
     private double sueldo;
     private static int contadorEmpleado;
     
-    public Empleado(String nombre, double sueldo){
-        super(nombre); // se usa el segundo constructor de la clase padre Persona
+    public Empleado(){
+        // super(); super pude o no ser escrita ya que implicitamente se invoca a super de la clase padre
         this.idEmpleado = ++contadorEmpleado;
+    }
+    
+    public Empleado(String nombre, double sueldo){
+        //super(nombre); // se usa el segundo constructor de la clase padre Persona
+        this(); // en un constructor no se puede llamar a super y this juntos solo a uno, este this manda a llamar al constructor vacio
+                //  de la parte superior
+        this.nombre = nombre;
         this.sueldo = sueldo;
     }
     
