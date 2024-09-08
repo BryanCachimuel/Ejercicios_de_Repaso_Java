27229fcc96e4e42f6d92a91bcf870e42bf3d504d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-07 21:39:22 -0500

    Realizando una prueba del funcionamiento de la clase cliente y su herencia de la clase padre Persona

--- CursoJava/src/Herencia/TestHerencia.java
+++ CursoJava/src/Herencia/TestHerencia.java
@@ -1,10 +1,22 @@
 package Herencia;
 
+import java.util.Date;
+
 public class TestHerencia {
     
     public static void main(String[] args) {
         Empleado empleado = new Empleado("Nelson", 600.00);
         System.out.println("Empleado: " + empleado);
+        
+        Empleado empleado2 = new Empleado("Franklin", 800.00);
+        System.out.println("Empleado: " + empleado2);
+        
+        System.out.println("\n");
+        
+        Date fecha = new Date();
+        
+        Cliente cliente = new Cliente("Kevin", 'M', 28, "Ibarra", fecha, true);
+        System.out.println("Cliente: " + cliente);
     }
     
 }
