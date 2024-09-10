ff155c7f0737185a18383829da146faa0c341de4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-09 20:50:00 -0500

    Creando el proyecto del formato de paquetes en java

---
+++ Paquetes/src/ec/com/formapaquetes/Utiileria.java
@@ -0,0 +1,12 @@
+package ec.com.formapaquetes;
+
+/*
+    Formato de paquetes en Java
+ */
+public class Utiileria {
+    
+    public static void imprimir(String s){
+        System.out.println("s = " + s);
+    }
+    
+}
