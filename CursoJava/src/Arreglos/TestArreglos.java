a02576f0a378997edda7b7b3a5ec7c41a7b25011
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-09 21:53:58 -0500

    Creando la estructura y proceso de uso de los arreglos

---
+++ CursoJava/src/Arreglos/TestArreglos.java
@@ -0,0 +1,19 @@
+package Arreglos;
+
+/*
+    Arreglos
+ */
+public class TestArreglos {
+    
+    public static void main(String[] args) {
+         int edades[] = new int[3];
+         edades[0] = 20;
+         edades[1] = 25;
+         edades[2] = 29;
+         
+         System.out.println("Edad 1: " + edades[0]);
+         System.out.println("Edad 2: " + edades[1]);
+         System.out.println("Edad 3: " + edades[2]);
+    }
+    
+}
