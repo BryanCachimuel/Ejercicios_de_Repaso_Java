06efd33d58ae794bbe405976f05388b7f7714e3f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-12 22:20:01 -0500

    Creando el método para mostrar por consola la impresión de la orden

--- MundoPC/src/ec/com/ib/mundopc/Orden.java
+++ MundoPC/src/ec/com/ib/mundopc/Orden.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.com.ib.mundopc;
 
-/**
- *
- * @author Bryan
- */
 public class Orden {
     
+    private final int idOrden;
+    private Computadora computadoras[];
+    private static int contadorOrdenes;
+    private static final int MAX_COMPUTADORAS = 10;
+    private int contadorComputadoras;
+    
+    public Orden(){
+        this.idOrden = ++contadorComputadoras;
+        this.computadoras = new Computadora[MAX_COMPUTADORAS];
+    }
+    
+    public void agregarComputadora(Computadora computadora){
+        if(this.contadorComputadoras < MAX_COMPUTADORAS){
+            this.computadoras[this.contadorComputadoras++] = computadora;
+        }
+        else{
+            System.out.println("Has superado el limite: " + MAX_COMPUTADORAS);
+        }
+    }
+    
+    public void mostrarOrden(){
+        System.out.println("Orden: #: " + this.idOrden);
+        System.out.println("Computadoras de la orden #: " +this.idOrden);
+        for(int  i = 0; i < this.contadorComputadoras; i++){
+            System.out.println(this.computadoras[i]);
+        }
+    }
+   
 }
