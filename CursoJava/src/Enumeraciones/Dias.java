7cb37600788fbb22b5a39ec0c3246baa84c7aff5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-13 21:02:06 -0500

    Creando la clase enum Dias con los dias de la semana ya que son enumerados

---
+++ CursoJava/src/Enumeraciones/Dias.java
@@ -0,0 +1,19 @@
+package Enumeraciones;
+
+/*
+    Manejo de enumeraciones (enum)
+    Es un tipo similar a una clase, pero es una clase particular que 
+    contiene varios elementos. 
+
+    Lo interesante de estos elementos es que son elementos publicos, static y final
+    así que todos los elementos son constantes en java
+ */
+public enum Dias {
+    LUNES, 
+    MARTES, 
+    MIERCOLES, 
+    JUEVES, 
+    VIERNES, 
+    SABADO, 
+    DOMINGO;
+}
