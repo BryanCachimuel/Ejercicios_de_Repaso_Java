483ac3dc668af8a534196d80d13efb008331a76a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-13 21:17:07 -0500

    Prueba de funcionamiento de la clase enum Dias para verificar el funcionamiento de los elementos de la clase enum Dias

--- CursoJava/src/Enumeraciones/TestEnumeraciones.java
+++ CursoJava/src/Enumeraciones/TestEnumeraciones.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Enumeraciones;
 
-/**
- *
- * @author Bryan
- */
 public class TestEnumeraciones {
     
+    public static void main(String[] args) {
+        System.out.println("Día 1: " + Dias.LUNES);
+        
+        indicarDiaSemana(Dias.DOMINGO);
+    }
+    
+    private static void indicarDiaSemana(Dias dias){
+        switch(dias){
+            case LUNES:
+                System.out.println("Primer Día de la Semana");
+                break;
+             
+            case MARTES:
+                System.out.println("Segundo Día de la Semana");
+                break;
+                
+            case MIERCOLES:
+                System.out.println("Tercer Día de la Semana");
+                break;
+            
+            case JUEVES:
+                System.out.println("Cuarto Día de la Semana");
+                break;
+                
+            case VIERNES:
+                System.out.println("Quinto Día de la Semana");
+                break;
+                
+            case SABADO:
+                System.out.println("Sexto Día de la Semana");
+                break;
+                
+            case DOMINGO:
+                System.out.println("Septimo Día de la Semana");
+                break;
+                
+            default:
+                System.out.println("No está el día que usted indica");
+        }
+    }
 }
