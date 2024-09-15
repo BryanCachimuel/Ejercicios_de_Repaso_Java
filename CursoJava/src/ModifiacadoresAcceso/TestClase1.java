b2dd089e33cfe529f84f91b73c67fe044efe6cd4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-14 21:55:32 -0500

    Realizando el test del uso del modificador de acceso protected

--- CursoJava/src/ModifiacadoresAcceso/TestClase1.java
+++ CursoJava/src/ModifiacadoresAcceso/TestClase1.java
@@ -3,9 +3,21 @@ package ModifiacadoresAcceso;
 public class TestClase1 {
     
     public static void main(String[] args) {
-        Clase1 clase1 = new Clase1();
+        Clase1 clase1 = new Clase1("Público");
         System.out.println("clase1 = " + clase1.atributoPublico);
         clase1.metodoPublico();
+        
+        /*
+        Si esta clase esta fuera del paquete de la clase a la que invocamos esto no funcionara y dará error
+        
+        System.out.println("clase1 = " + clase1.atributoProtected);
+        clase1.metodoProtected();
+        */ 
+        
+        System.out.println("\n");
+        
+        ClaseHija claseHija = new ClaseHija();
+        System.out.println("claseHija = " + claseHija);
     }
     
 }
