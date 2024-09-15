766fa47a273363a8469d316b5af1b95745fc5b0a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-14 21:51:40 -0500

    Definiendo un atributo, constructor y método de tipo protected para analizar el funcionamiento de este modificador de acceso

--- CursoJava/src/ModifiacadoresAcceso/Clase1.java
+++ CursoJava/src/ModifiacadoresAcceso/Clase1.java
@@ -7,11 +7,21 @@ public class Clase1 {
     
     public String atributoPublico = "Valor atributo público";
     
-    public Clase1(){
+    public Clase1(String args){
         System.out.println("Constructor Público");
     }
     
     public void metodoPublico(){
         System.out.println("Método Público");
+    }
+    
+    protected String atributoProtected = "Valor atributo protected";
+    
+    protected Clase1(){
+        System.out.println("Constructor Protected");
+    }
+    
+    protected void metodoProtected(){
+        System.out.println("Método Protected");
     }
 }
