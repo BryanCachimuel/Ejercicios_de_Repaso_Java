eb719fd7176cbb7fbce9c95ee6e670522b830c37
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-16 21:17:35 -0500

    Agregando en el constructor los procedimientos de acceso para los atributos privados cosa que es imposible por sus leyes privadas

--- CursoJava/src/ModifiacadoresAcceso/ClaseHija.java
+++ CursoJava/src/ModifiacadoresAcceso/ClaseHija.java
@@ -7,6 +7,18 @@ public class ClaseHija extends Clase1{
         this.atributoProtected = "Modificación atributo protected";
         System.out.println("atributo protegido = " + this.atributoProtected);
         this.metodoProtected();
+        System.out.println("\n");
+        this.atributoDefault = "Modificación atributo default";
+        System.out.println("atributo default = " + this.atributoDefault);
+        this.metodoDefault();
+        
+        System.out.println("\n");
+        
+        /*
+        No se puede acceder a los atributos y métodos privados
+        this.atributoPrivado = "Modificación de atributo Privado";
+        this.metodoPrivado();
+        */
     }
     
 }
