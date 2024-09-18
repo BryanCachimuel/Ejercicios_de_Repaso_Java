23579636ba3e58356c0a6ff4f5e2307964fda706
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-17 21:05:24 -0500

    Creando el método determinarTipo en donde se realizaran validaciones con instanceof con las clases Gerente, Empleado y Object

--- CursoJava/src/InstanceOf/TestInstanceOf.java
+++ CursoJava/src/InstanceOf/TestInstanceOf.java
@@ -1,21 +1,36 @@
-package SobreescrituraMetodos;
+package InstanceOf;
 
-public class TestSobreescritura {
+import SobreescrituraMetodos.*;
+
+public class TestInstanceOf {
     
     public static void main(String[] args) {
         
         Empleado empleado = new Empleado("Nelson Cacoango", 750.00);
-        //System.out.println("empleado = " + empleado.obtenerDetalles());
-        imprimir(empleado);
+        determinarTipo(empleado);
         
-        Gerente gerente = new Gerente("Lennin", 600.00, "Sistemas");  
-        //System.out.println("Gerente = " + gerente.obtenerDetalles());
-        imprimir(gerente);
+        Gerente gerente = new Gerente("Lennin", 600.00, "Sistemas"); 
+        //determinarTipo(gerente);
         
     }
     
     // proceso de polimorfismo
-    public static void imprimir(Empleado empleado){
-        System.out.println("empleado = " + empleado.obtenerDetalles());
+    public static void determinarTipo(Empleado empleado){
+       if(empleado instanceof Gerente){
+           System.out.println("Es de tipo Geremte");
+           Gerente gerente = (Gerente)empleado;
+           System.out.println(gerente.getDepartamento());
+           ((Gerente) empleado).getDepartamento();
+       }
+       else if(empleado instanceof Empleado){
+           System.out.println("Es de tipo Empleado");
+           //Gerente gerente = (Gerente)empleado;
+           //System.out.println(gerente.getDepartamento());
+           System.out.println("empleado = " + empleado.getNombre());
+       }
+       else if(empleado instanceof Object){
+           System.out.println("Es de tipo Object");
+           empleado.toString();
+       }
     }
 }
