9b9270b3c315566ebf49ebaea4d0d10094f10be4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-17 21:59:23 -0500

    Agregando un método get para el atributo de la clase Escritor

--- CursoJava/src/ConversionObjetos/Escritor.java
+++ CursoJava/src/ConversionObjetos/Escritor.java
@@ -2,16 +2,24 @@ package ConversionObjetos;
 
 public class Escritor extends Empleado{
     
-    final TipoEscritura tipoEscritua;
+    final TipoEscritura tipoEscritura;
     
     public Escritor(String nombre, double sueldo, TipoEscritura tipoEscritura){
         super(nombre, sueldo);
-        this.tipoEscritua = tipoEscritura;
+        this.tipoEscritura = tipoEscritura;
     }
 
     @Override
     public String obtenerDetalles() {
-        return super.obtenerDetalles() + ", tipoEscritura: " + tipoEscritua.getDescripcion();
+        return super.obtenerDetalles() + ", tipoEscritura: " + tipoEscritura.getDescripcion();
     }
-    
+
+    @Override
+    public String toString() {
+        return "Escritor{" + "tipoEscritua=" + tipoEscritura + '}' + " " + super.toString();
+    }
+
+    public TipoEscritura getTipoEscritura() {
+        return this.tipoEscritura;
+    }
 }
