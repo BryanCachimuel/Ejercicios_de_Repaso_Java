6deefc9c7606f01d17270fd7eab3d8ebedea0f61
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-18 20:36:32 -0500

    Realizando un tets comparativo entre instancias de los objetos empleados

--- CursoJava/src/ClaseObject/TestClaseObject.java
+++ CursoJava/src/ClaseObject/TestClaseObject.java
@@ -3,7 +3,15 @@ package ClaseObject;
 public class TestClaseObject {
     
     public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Franklin", 500.00);
+        Empleado empleado2 = new Empleado("Franklin", 500.00);
         
+        if(empleado1 == empleado2){
+            System.out.println("Tiene la misma referencia en memoria");
+        }
+        else{
+            System.out.println("Tienen distinta referencia en memoria");
+        }
     }
     
 }
