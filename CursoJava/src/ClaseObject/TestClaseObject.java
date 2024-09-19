08e33118ceca5101f4658ef3d9bdea05b37b2ea9
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-18 20:46:22 -0500

    Realizando una validación entre objetos mediante los métodos equals y hascode

--- CursoJava/src/ClaseObject/TestClaseObject.java
+++ CursoJava/src/ClaseObject/TestClaseObject.java
@@ -4,13 +4,25 @@ public class TestClaseObject {
     
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Franklin", 500.00);
-        Empleado empleado2 = new Empleado("Franklin", 500.00);
+        Empleado empleado2 = new Empleado("Nelson", 500.00);
         
         if(empleado1 == empleado2){
             System.out.println("Tiene la misma referencia en memoria");
         }
         else{
             System.out.println("Tienen distinta referencia en memoria");
+        }
+        
+        if(empleado1.equals(empleado2)){
+            System.out.println("Los objetos son iguales en contenido");
+        }else{
+            System.out.println("Los objetos no son iguales en contenido");
+        }
+        
+        if(empleado1.hashCode() == empleado2.hashCode()){
+            System.out.println("El valor hascode es igual");
+        }else{
+            System.out.println("El valor hashcode no es igual");
         }
     }
     
