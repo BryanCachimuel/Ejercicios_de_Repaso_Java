0867abb1b0938513763229afa359fe136e09f474
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-19 21:01:17 -0500

    Realizando una prueba de las clases abstractas

---
+++ CursoJava/src/ClasesAbstractas/TestClasesAbstractas.java
@@ -0,0 +1,12 @@
+package ClasesAbstractas;
+
+public class TestClasesAbstractas {
+    
+    public static void main(String[] args) {
+        
+        FiguraGeometrica figura = new Rectangulo("Rectangulo Obtuso");
+        figura.dibujar();
+        
+    }
+    
+}
