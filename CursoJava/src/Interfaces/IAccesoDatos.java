ca57554d05d7653516b2092ac4ff6170f5c93977
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-19 21:07:55 -0500

    Creando una constante y los métodos estáticos de la interfaz

--- CursoJava/src/Interfaces/IAccesoDatos.java
+++ CursoJava/src/Interfaces/IAccesoDatos.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interfaces;
 
-/**
- *
- * @author Bryan
+/*
+    Manejo de Interfaces en Java
  */
 public interface IAccesoDatos {
+    
+    int MAX_REGISTRO = 10;
+    
+    void insertar();
+    void listar();
+    void actualizar();
+    void eliminar();
     
 }
