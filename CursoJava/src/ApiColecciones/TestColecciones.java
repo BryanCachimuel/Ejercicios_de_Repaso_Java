02c7d209451c29a46c269f195d4a939d4a003585
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-20 21:58:09 -0500

    Probando los procedimientos de uso para listas y sets

--- CursoJava/src/ApiColecciones/TestColecciones.java
+++ CursoJava/src/ApiColecciones/TestColecciones.java
@@ -16,15 +16,29 @@ public class TestColecciones {
         miLista.add("Viernes");
         miLista.add("Sábado");
         miLista.add("Domingo");
+        miLista.add("Domingo"); // se puede duplicar los elementos con el List
         
-        /* Las dos formas imprimen el mismo resultado */
-        for(Object elemento : miLista){
+        imprimir(miLista);
+        
+        /* Se imprimen los elementos se importar el orden dado por el usuario */
+        Set miSet = new HashSet();
+        miSet.add("Lunes");
+        miSet.add("Miercóles");
+        miSet.add("Viernes");
+        miSet.add("Viernes"); // no se puede duplicar elementos con el Set
+        miSet.add("Domingo");
+        
+        imprimir(miSet);
+    }
+    
+    public static void imprimir(Collection coleccion){
+        /* Las dos formas imprimen el mismo resultado 
+        for(Object elemento : coleccion){
             System.out.println("elemento = " + elemento);
-        }
+        }*/
         
-        miLista.forEach(elemento -> {
+        coleccion.forEach(elemento -> {
             System.out.println("elemento = " + elemento);
         });
     }
-    
 }
