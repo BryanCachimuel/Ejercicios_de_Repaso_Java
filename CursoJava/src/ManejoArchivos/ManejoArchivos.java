7fd2616c706af66ab53cb3891a188c6957eb31cb
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-21 22:07:46 -0500

    Creando el ejercicio de manejo de archivos

---
+++ CursoJava/src/ManejoArchivos/ManejoArchivos.java
@@ -0,0 +1,23 @@
+package ManejoArchivos;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+    Manejo de archivos
+ */
+public class ManejoArchivos {
+    
+    public static void crearArchivo(String nombreArchivo) {
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (FileNotFoundException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+}
