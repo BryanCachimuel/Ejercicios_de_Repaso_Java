f3539408c9a1dbddf1166743368ec1de46494b02
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-22 21:47:29 -0500

    Creando el scipt para los métodos estáticos crearArchivo y escribirArchivo

--- CursoJava/src/ManejoArchivos/ManejoArchivos.java
+++ CursoJava/src/ManejoArchivos/ManejoArchivos.java
@@ -20,4 +20,15 @@ public class ManejoArchivos {
         }
     }
     
+    public static void escribirArchivo(String nombreArchivo, String contenido){
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            salida.close();
+            System.out.println("Se ha escrito en el archivo");
+        } catch (FileNotFoundException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
 }
