bd7bab5304a87e19f433091c876ae305c363cad2
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-09-22 22:00:47 -0500

    Realizando pruebas de funcionamiento del método estático anexarArchivo

--- CursoJava/src/ManejoArchivos/TestManejoArchivos.java
+++ CursoJava/src/ManejoArchivos/TestManejoArchivos.java
@@ -8,6 +8,9 @@ public class TestManejoArchivos {
         var nombreArchivo = "prueba.txt";
         //crearArchivo(nombreArchivo);
         escribirArchivo(nombreArchivo, "Texto referencial de escritura en el archivo");
+        /*escribirArchivo(nombreArchivo, "agregando mas texto para el archivo");*/
+        anexarArchivo(nombreArchivo, "Texto referencias para anexar información");
+        anexarArchivo(nombreArchivo, "anexando otra línea al escrito");
     }
     
 }
