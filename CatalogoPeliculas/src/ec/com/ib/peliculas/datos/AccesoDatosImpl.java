86263e71cf9b14e56f8dabb578c9e9eb01fed16e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-03 21:15:50 -0500

    Creando los métodos para crear y borrar un archivo

--- CatalogoPeliculas/src/ec/com/ib/peliculas/datos/AccesoDatosImpl.java
+++ CatalogoPeliculas/src/ec/com/ib/peliculas/datos/AccesoDatosImpl.java
@@ -4,13 +4,11 @@ import ec.com.ib.peliculas.domain.Pelicula;
 import ec.com.ib.peliculas.excepciones.*;
 import java.io.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class AccesoDatosImpl implements IAccesoDatos{
 
     @Override
-    public boolean existe(String nombreRecurso) throws AccesoDatosEx {
+    public boolean existe(String nombreRecurso){
         File archivo = new File(nombreRecurso);
         return archivo.exists();
     }
@@ -76,7 +74,7 @@ public class AccesoDatosImpl implements IAccesoDatos{
             ex.printStackTrace();
             throw new LecturaDatosEx("Excepcion al buscar peliculas: " + ex.getMessage());
         } catch (IOException ex) {
-             ex.printStackTrace();
+            ex.printStackTrace();
             throw new LecturaDatosEx("Excepcion al buscar peliculas: " + ex.getMessage());
         }
         
@@ -85,12 +83,23 @@ public class AccesoDatosImpl implements IAccesoDatos{
 
     @Override
     public void crear(String nombreRecurso) throws AccesoDatosEx {
-        
+        File archivo = new File(nombreRecurso);
+        try {
+            PrintWriter salida = new PrintWriter(new FileWriter(archivo));
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new AccesoDatosEx("Excepcion al crear archivo: " + ex.getMessage());
+        }
     }
 
     @Override
-    public void borrar(String nombreRecurso) throws AccesoDatosEx {
-        
+    public void borrar(String nombreRecurso){
+        File archivo = new File(nombreRecurso);
+        if(archivo.exists())
+            archivo.delete();
+        System.out.println("Se ha borrado el archivo");
     }
  
     
