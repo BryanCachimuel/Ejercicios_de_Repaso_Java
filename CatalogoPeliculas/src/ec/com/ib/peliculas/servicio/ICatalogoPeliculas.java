fec7b5c3d5671af1f78756ed3eb2499fc87d97fa
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-04 19:44:50 -0500

    Creando la interfaz ICatalogoPeliculas con su constante y métofos estáticos

--- CatalogoPeliculas/src/ec/com/ib/peliculas/servicio/ICatalogoPeliculas.java
+++ CatalogoPeliculas/src/ec/com/ib/peliculas/servicio/ICatalogoPeliculas.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.com.ib.peliculas.servicio;
 
-/**
- *
- * @author Bryan
- */
 public interface ICatalogoPeliculas {
+    
+    String NOMBRE_RECURSO = "peliculas.txt";
+    
+    void agregarPelicula(String nombrePelicula);
+    void listarPeliculas();
+    void buscarPelicula(String buscar);
+    void iniciarCatalogoPeliculas();
     
 }
