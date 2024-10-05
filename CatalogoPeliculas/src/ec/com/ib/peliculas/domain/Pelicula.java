49134c2a31009cafee243327886e1fe3dfbf46f8
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-04 19:43:32 -0500

    Mejorando el método toString para que solo imprima el nombre de la película

--- CatalogoPeliculas/src/ec/com/ib/peliculas/domain/Pelicula.java
+++ CatalogoPeliculas/src/ec/com/ib/peliculas/domain/Pelicula.java
@@ -21,7 +21,7 @@ public class Pelicula {
 
     @Override
     public String toString() {
-        return "Pelicula{" + "nombre=" + nombre + '}';
+        return "El nombre de la película es: " + this.nombre;
     }
     
 }
