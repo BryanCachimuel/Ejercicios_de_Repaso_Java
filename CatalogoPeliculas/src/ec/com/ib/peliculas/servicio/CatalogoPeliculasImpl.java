621bb8438a9c4e86c96ca5c33a03d755378ce2f6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-04 20:04:20 -0500

    Codificando la funcionalidad para los métodos: listarPeliculas, buscarPelicula e iniciarCatalogoPeliculas

--- CatalogoPeliculas/src/ec/com/ib/peliculas/servicio/CatalogoPeliculasImpl.java
+++ CatalogoPeliculas/src/ec/com/ib/peliculas/servicio/CatalogoPeliculasImpl.java
@@ -4,8 +4,6 @@ import ec.com.ib.peliculas.datos.AccesoDatosImpl;
 import ec.com.ib.peliculas.datos.IAccesoDatos;
 import ec.com.ib.peliculas.domain.Pelicula;
 import ec.com.ib.peliculas.excepciones.AccesoDatosEx;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class CatalogoPeliculasImpl implements ICatalogoPeliculas{
     
@@ -31,17 +29,43 @@ public class CatalogoPeliculasImpl implements ICatalogoPeliculas{
 
     @Override
     public void listarPeliculas() {
-        
+        try {
+            var peliculas = this.datos.listar(NOMBRE_RECURSO);
+            for(var pelicula : peliculas){
+                System.out.println("pelicula: " + pelicula);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
     public void buscarPelicula(String buscar) {
-        
+        String resultado = null;
+        try {
+            resultado = this.datos.buscar(NOMBRE_RECURSO, buscar);
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
+        System.out.println("resultado: " + resultado);
     }
 
     @Override
     public void iniciarCatalogoPeliculas() {
-        
+        try {
+            if(this.datos.existe(NOMBRE_RECURSO)){
+                datos.borrar(NOMBRE_RECURSO);
+                datos.crear(NOMBRE_RECURSO);
+            }
+            else{
+                datos.crear(NOMBRE_RECURSO);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error al Iniciar Catálogo de Películas");
+            ex.printStackTrace(System.out);
+        }
     }
     
 }
