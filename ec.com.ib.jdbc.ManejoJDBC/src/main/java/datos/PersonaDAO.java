db485e71353495a0e2583d0921d274fba6f07997
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-05 22:14:41 -0500

    Creando el método para listar todos los registros de la base de datos

---
+++ ec.com.ib.jdbc.ManejoJDBC/src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,41 @@
+package datos;
+
+import domain.Persona;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PersonaDAO {
+    
+    private static final String SQL_SELECT = "SELECT id_persona,nombre,apellido,email,telefono FROM persona";
+    
+    public List<Persona> seleccionar(){
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        ResultSet rs = null;
+        Persona persona;
+        List<Persona> personas = new ArrayList<>();
+        
+        try {
+            conn = Conexion.getConnection();
+            psmtm = conn.prepareStatement(SQL_SELECT);
+            rs = psmtm.executeQuery();
+            while(rs.next()){
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+                
+                // se usa el constructor con toda la información para agregar una persona
+                persona = new Persona(idPersona, nombre, apellido, email, telefono);
+                personas.add(persona);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        return personas;
+    }
+    
+}
