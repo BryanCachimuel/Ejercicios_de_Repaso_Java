3fc12d9a813e8579d8c3e5e12c6da524b4fd9571
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-06 21:19:20 -0500

    Realizando un test de funcionamiento del método registrar para probar el funcionamiento del método

--- ec.com.ib.jdbc.ManejoJDBC/src/main/java/test/TestManejoPersonas.java
+++ ec.com.ib.jdbc.ManejoJDBC/src/main/java/test/TestManejoPersonas.java
@@ -8,16 +8,25 @@ public class TestManejoPersonas {
     
     public static void main(String[] args) {
         PersonaDAO personaDao = new PersonaDAO();
+        
+        
+        /* se puede usar los dos procesos
+         personas.forEach(persona -> {
+            System.out.println("Persona: " + persona);
+        });
+        for(Persona persona : personas){
+            System.out.println("Persona: " + persona);
+        }*/
+        
+        /* Insertando un nuevo objeto de tipo persona */
+        Persona personaNueva = new Persona("Richard", "Tarupí", "rftc@gmail.com", "0974365872");
+        personaDao.insertar(personaNueva);
+        
         List<Persona> personas = personaDao.seleccionar();
         /* es recomendable usar una función lamda */
         personas.forEach(persona -> {
             System.out.println("Persona: " + persona);
         });
-        
-        /* se puede usar los dos procesos
-        for(Persona persona : personas){
-            System.out.println("Persona: " + persona);
-        }*/
     }
     
 }
