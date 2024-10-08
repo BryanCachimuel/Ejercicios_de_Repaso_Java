b7a31791cad3e7f8b3363db86b4d995909ac7ee0
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-07 21:03:32 -0500

    Realizando un test de funcionamiento del método listarUsuarios para traer toda la información de los registros de los usuarios registrados

---
+++ UsuarioJDBC/src/main/java/test/TestManejoUsuarios.java
@@ -0,0 +1,20 @@
+package test;
+
+import datos.UsuarioDAO;
+import domain.Usuario;
+import java.util.List;
+
+public class TestManejoUsuarios {
+    
+    public static void main(String[] args) {
+        
+        UsuarioDAO usuarioDao = new UsuarioDAO();
+        List<Usuario> usuarios = usuarioDao.listarUsuarios();
+        
+        for(Usuario usuario : usuarios){
+            System.out.println("Usuarios: " + usuario);
+        }
+        
+    }
+    
+}
