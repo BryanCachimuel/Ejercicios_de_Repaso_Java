c9480ed63f9c1868a535822c7c08348a48a8fee1
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-07 21:33:05 -0500

    Creando los métodos para actualizar y eliminar registros de la base de datos test para la entidad usuario

--- UsuarioJDBC/src/main/java/datos/UsuarioDAO.java
+++ UsuarioJDBC/src/main/java/datos/UsuarioDAO.java
@@ -11,6 +11,8 @@ public class UsuarioDAO {
     
     private static final String SQL_SELECT = "SELECT id_usuario, usuario, password FROM usuario";
     private static final String SQL_INSERT = "INSERT INTO usuario(usuario, password) VALUES(?,?)";
+    private static final String SQL_UPDATE = "UPDATE usuario SET usuario = ?, password = ? WHERE id_usuario = ?";
+    private static final String SQL_DELETE = "DELETE FROM usuario WHERE id_usuario = ?";
     
     public List<Usuario> listarUsuarios(){
         Connection conn = null;
@@ -69,4 +71,51 @@ public class UsuarioDAO {
         return registros;
     }
     
+    public int actualizar(Usuario usuario){
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        int registroActualizado = 0;
+        
+        try {
+            conn = getConnection();
+            psmtm = conn.prepareStatement(SQL_UPDATE);
+            psmtm.setString(1, usuario.getUsuario());
+            psmtm.setString(2, usuario.getPassword());
+            psmtm.setInt(3, usuario.getIdUsuario());
+            registroActualizado = psmtm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            try {
+                close(psmtm);
+                close(conn);
+            } catch (SQLException ex) {
+               ex.printStackTrace(System.out);
+            }
+        }
+        return registroActualizado;
+    }
+    
+    public int eliminar(Usuario usuario){
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        int registroEliminar = 0;
+        
+        try {
+            conn = getConnection();
+            psmtm = conn.prepareStatement(SQL_DELETE);
+            psmtm.setInt(1, usuario.getIdUsuario());
+            registroEliminar = psmtm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            try {
+                close(psmtm);
+                close(conn);
+            } catch (SQLException ex) {
+               ex.printStackTrace(System.out);
+            }
+        }
+        return registroEliminar;
+    }
 }
