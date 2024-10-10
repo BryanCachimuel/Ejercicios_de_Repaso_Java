84cf742aa603551b2b8e117082da775d7e349586
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-09 20:59:57 -0500

    Creando un Servlet el cual posee un método doGet el cual tiene un decorador el cual indica como debe acceder el navegador a este servlet

--- HolaMundoServlets/src/main/java/web/HolaMundo.java
+++ HolaMundoServlets/src/main/java/web/HolaMundo.java
@@ -1,5 +1,19 @@
 package web;
 
-public class HolaMundo {
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+/* para reconocer desde el navegador se debe agregar el siguiente decorador y dentro de los parentesis se define el path para ingresar al servlet*/
+@WebServlet("/HolaMundo")
+public class HolaMundo extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hola Mundo desde Servelets");
+    }
     
 }
