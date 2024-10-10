d2625b970083276ec49594bcd04e3b8c17271fc0
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-09 20:33:37 -0500

    Configurando el pool de conexiones para la clase donde se realiza la conexión hacia la base de datos

--- PoolConexionesManejoJDBC/src/main/java/datos/Conexion.java
+++ PoolConexionesManejoJDBC/src/main/java/datos/Conexion.java
@@ -1,6 +1,8 @@
 package datos;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
     
@@ -8,8 +10,20 @@ public class Conexion {
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin1994";
     
+    /*inicializa el pool de conexiones*/
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        // se define el tamaño inicial del pool de conexiones
+        ds.setInitialSize(5);
+        return ds;
+    }
+    
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        // se hace el llamado al método getDataSource que se encarga de inicializar el pool de conexiones
+        return getDataSource().getConnection();
     }
     
     public static void close(ResultSet rs) throws SQLException{
