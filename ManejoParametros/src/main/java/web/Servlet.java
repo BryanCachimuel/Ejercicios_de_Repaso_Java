529cf4c15da52815640a438947eef518f1b5e2cd
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-09 21:48:09 -0500

    Creando el método doPost para obtener la información que se agregue mediante el ingreso de información a travez de un formulario de registro

--- ManejoParametros/src/main/java/web/Servlet.java
+++ ManejoParametros/src/main/java/web/Servlet.java
@@ -1,7 +1,33 @@
 package web;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+
+@WebServlet("/Servlet")
 public class Servlet extends HttpServlet{
+    
+    /* Procesando los valores que vienen del formulario */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        // leer los parámetros del formulario html
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        System.out.println("usuario: " + usuario);
+        System.out.println("password: " + password);
+        
+        PrintWriter out = response.getWriter();
+        out.print("<html>");
+        out.print("<body>");
+        out.print("El parámetro de usuario es: " + usuario);
+        out.print("<br/>");
+        out.print("El parámetro de password es: " + password);
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
+    }
     
 }
