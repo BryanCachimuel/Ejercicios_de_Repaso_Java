a7c6bdb706dc79b846c76b3d5cb9a0aef14a3e21
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-13 20:43:37 -0500

    Creando el método doPost en donde se realiza el procesamiento de captura de los datos que se ingresen en formulario de registro

--- ManejoFormulariosHTML/src/main/java/web/Servlet.java
+++ ManejoFormulariosHTML/src/main/java/web/Servlet.java
@@ -1,9 +1,27 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        String tecnologias[] = request.getParameterValues("tecnologia");
+        String genero = request.getParameter("genero");
+        String ocupacion = request.getParameter("ocupacion");
+        String musica[] = request.getParameterValues("musica");
+        String comentario = request.getParameter("comentarios");
+        
+        
+    }
     
 }
