799dffdcf50cf69619041f2ac943f23a795a807d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-15 22:32:38 -0500

    Realizando un Servlet para obtener la hora actualizada por cada segundo

--- CabecerosRespuesta/src/main/java/web/HoraServlet.java
+++ CabecerosRespuesta/src/main/java/web/HoraServlet.java
@@ -1,9 +1,28 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
 
 @WebServlet("/HoraServlet")
 public class HoraServlet extends HttpServlet{
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("refresh", "1");
+        Date fecha = new Date();
+        
+        // dar formato a la hora
+        SimpleDateFormat formateador = new SimpleDateFormat("'Hora Actualizada' HH:mm:ss");
+        String horaConFormato = formateador.format(fecha);
+        
+        // desplegar la información al cliente
+        PrintWriter out = response.getWriter();
+        out.print("Hora Actualziada: " + horaConFormato);
+        out.close();
+    }
     
 }
