42b937ce63ec9f3ad6af049938e27dd32ce72a5a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-15 22:06:21 -0500

    Creando los servlets GeneracionExcelServlet y HoraServlet

---
+++ CabecerosRespuesta/src/main/java/web/GeneracionExcelServlet.java
@@ -0,0 +1,10 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/GeneracionExcelServlet")
+public class GeneracionExcelServlet extends HttpServlet{
+    
+    
+}
