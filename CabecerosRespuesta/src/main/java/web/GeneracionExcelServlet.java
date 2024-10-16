c03dd190478ba59697d69831a8d25b6ce9d08086
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-15 22:22:41 -0500

    Creando la funcionalidad del Servlet GeneracionExcelServlet en donde se puedo apreciar el uso de cabeceros de respuesta para el navegador, para no almacenar información en cache y para desplegar información del cliente

--- CabecerosRespuesta/src/main/java/web/GeneracionExcelServlet.java
+++ CabecerosRespuesta/src/main/java/web/GeneracionExcelServlet.java
@@ -1,10 +1,29 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
 
 @WebServlet("/GeneracionExcelServlet")
 public class GeneracionExcelServlet extends HttpServlet{
     
-    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        // definiendo el cabecero de respuesta para el navegador
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment;filename=excelEjemplo.xls");
+        
+        // cabeceros para que no se guarde información en el cache
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-control", "no-store");
+        response.setDateHeader("Expires", -1); // para que se genere siempre información nueva
+        
+        // desplegar la información al cliente
+        PrintWriter out = response.getWriter();
+        out.print("\tValores");
+        out.print("\t1");
+        out.print("\t2");
+        out.print("Total\t=SUMA(b2:b3)");
+        out.close();
+    }
 }
