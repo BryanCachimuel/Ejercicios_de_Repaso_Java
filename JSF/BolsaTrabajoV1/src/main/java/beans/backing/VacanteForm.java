ba78f7e7f39464f42230c50f2caccb97be567afe
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-28 21:35:42 -0500

    Agregando una validación extra para el método enviar para que se controle los datos que se ingresen para el apellido del candidato y realizando una impresión para mensajes globales

--- JSF/BolsaTrabajoV1/src/main/java/beans/backing/VacanteForm.java
+++ JSF/BolsaTrabajoV1/src/main/java/beans/backing/VacanteForm.java
@@ -2,6 +2,8 @@ package beans.backing;
 
 import beans.model.Candidato;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.apache.logging.log4j.LogManager;
@@ -15,13 +17,25 @@ public class VacanteForm {
     private Candidato candidato;
     
     Logger log = LogManager.getRootLogger();
+
+    public VacanteForm() {
+        log.info("Creando el objeto VacanteForm");
+    }
     
     public void setCandidato(Candidato candidato){
         this.candidato = candidato;
     }
     
     public String enviar(){
-        if(this.candidato.getNombre().equals("Juan")){
+        if(this.candidato.getNombre().equals("Lennin")){
+            if(this.candidato.getApellido().equals("Lotton")){
+                String msg = "Gracias, pero Lennin Lotton ya trabaja con nosotros";
+                FacesMessage facesMessager = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String componentId = null;          // este es un mensaje global
+                facesContext.addMessage(componentId, facesMessager);
+                return "index";
+            }
             log.info("Entrando al caso de exito");
             return "exito";
         }
