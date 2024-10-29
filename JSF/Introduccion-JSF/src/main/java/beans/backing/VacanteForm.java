02f69ef986d25c427a01e5c17410288c90ddf334
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-28 20:38:10 -0500

    Agregando mensajes para el control de errores con log4j dentro de la lógica del ejercicio

--- JSF/Introduccion-JSF/src/main/java/beans/backing/VacanteForm.java
+++ JSF/Introduccion-JSF/src/main/java/beans/backing/VacanteForm.java
@@ -3,6 +3,7 @@ package beans.backing;
 import beans.model.Candidato;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.*;
+import org.apache.logging.log4j.*;
 
 @Named
 @RequestScoped
@@ -11,14 +12,18 @@ public class VacanteForm {
     @Inject
     private Candidato candidato;
     
+    Logger log = LogManager.getRootLogger();
+    
     public void setCandidato(Candidato candidato){
         this.candidato = candidato;
     }
     
     public String enviar(){
         if(this.candidato.getNombre().equals("Juan")){
+            log.info("Entrando al caso de exito");
             return "exito";
         }else{
+            log.info("Entrando al caso de fallo");
             return "fallo";
         }
     }
--- JSF/Introduccion-JSF/src/main/java/beans/model/Candidato.java
+++ JSF/Introduccion-JSF/src/main/java/beans/model/Candidato.java
@@ -2,6 +2,7 @@ package beans.model;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import org.apache.logging.log4j.*;
 
 /*
     Lo más importante aquí son las anotaciones para con esto pueda ser reconocido por el framework de Java Server Faces
@@ -12,6 +13,8 @@ import javax.inject.Named;
 @RequestScoped  // se utiliza el alcance RequestScoped
 public class Candidato {
     private String nombre = "Introduce tu nombre";
+    
+    Logger log = LogManager.getRootLogger();
 
     public String getNombre() {
         return this.nombre;
@@ -19,6 +22,7 @@ public class Candidato {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+        log.info("Modificando la propiedad de nombre: " + this.nombre);
     }
     
     
