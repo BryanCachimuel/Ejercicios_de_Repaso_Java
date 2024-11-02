7509f31731af09e805e92d930ccc386f054b0eec
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-01 21:04:20 -0500

    Realizando un proyecto introductorio a lo que es la aplicación de la tecnología JPA en java

---
+++ JPA/IntroduccionJPA/src/main/java/IntroduccionJPA.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Bryan
+ */
+public class IntroduccionJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
