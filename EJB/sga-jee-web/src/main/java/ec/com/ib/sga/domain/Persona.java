1d0d04ad972ba778b9834c0d83df73f5e843d883
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-03 20:18:03 -0500

    Integrando la tecnología JPA a la clase Persona para que se convierta en Entidad y mapee todos los atributos de clase conjuntamente con la entidad persona y sus atributos de la base de datos y definiendo el archivo log4j para obtener mensajes definidos bajo consola

--- EJB/sga-jee-web/src/main/java/ec/com/ib/sga/domain/Persona.java
+++ EJB/sga-jee-web/src/main/java/ec/com/ib/sga/domain/Persona.java
@@ -1,26 +1,37 @@
 package ec.com.ib.sga.domain;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.*;
 
 /*
-    SEGUNDO PASO: Se crea la clase Persona, para enviarse del servidor al cliente esta clase necesita serializarse mediante su implementación a esta clase
+    Esta clase se va crear como una ENTIDAD para poder interactuar con el api de JPA hacia la base de datos y esto se hace
+    mediante la notación @Entity
 */
 
+@Entity
 public class Persona implements Serializable{
     
-    private static final long serialVersionUID = 1L;        // atributo requerido por la interfaz Serializable
-    
+    private static final long serialVersionUID = 1L;  // esta variable es una implementación necesaria que viene de la interface Serializable
+                                                        
+    @Id                                                 // Anotacion que indetifica a idPersona como la clave primaría
+    @GeneratedValue(strategy=GenerationType.IDENTITY)  // Anotación que dice que es un atributo autoincremental dentro de la base de datos
+    @Column(name="id_persona")                         // mapeo del atributo id_persona desde la bdd con idPersona dentro de esta clase 
     private int idPersona;
+    
     private String nombre;
+    
     private String apellido;
+    
     private String email;
+    
     private String telefono;
 
+    /* Contructores */
     public Persona() {
     }
 
-    public Persona(int idPersona, String nombre, String apellido, String email, String telefono) {
-        this.idPersona = idPersona;
+    public Persona(String nombre, String apellido, String email, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
@@ -70,7 +81,7 @@ public class Persona implements Serializable{
     @Override
     public String toString() {
         return "Persona: " + "idPersona=" + idPersona 
-                           + ", nombre=" + nombre 
+                           + ", nombre=" + nombre
                            + ", apellido=" + apellido 
                            + ", email=" + email 
                            + ", telefono=" + telefono;
