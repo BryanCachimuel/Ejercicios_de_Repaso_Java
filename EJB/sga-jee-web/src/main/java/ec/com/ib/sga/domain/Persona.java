f5bca651372d1371d666e1af6d377401c43e0ece
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-03 20:26:56 -0500

    Integrando la tecnología JPA a la clase Persona para que se convierta en Entidad y mapee todos los atributos de clase conjuntamente con la entidad persona y sus atributos de la base de datos y definiendo la unidad de persistencia donde se agregan todas las clases de persistencia del proyecto y se realiza la configuración de la conexión hacia la base de datos

--- EJB/sga-jee-web/src/main/java/ec/com/ib/sga/domain/Persona.java
+++ EJB/sga-jee-web/src/main/java/ec/com/ib/sga/domain/Persona.java
@@ -7,24 +7,23 @@ import javax.persistence.*;
 /*
     Esta clase se va crear como una ENTIDAD para poder interactuar con el api de JPA hacia la base de datos y esto se hace
     mediante la notación @Entity
-*/
-
+ */
 @Entity
-public class Persona implements Serializable{
-    
+public class Persona implements Serializable {
+
     private static final long serialVersionUID = 1L;  // esta variable es una implementación necesaria que viene de la interface Serializable
-                                                        
+
     @Id                                                 // Anotacion que indetifica a idPersona como la clave primaría
-    @GeneratedValue(strategy=GenerationType.IDENTITY)  // Anotación que dice que es un atributo autoincremental dentro de la base de datos
-    @Column(name="id_persona")                         // mapeo del atributo id_persona desde la bdd con idPersona dentro de esta clase 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Anotación que dice que es un atributo autoincremental dentro de la base de datos
+    @Column(name = "id_persona")                         // mapeo del atributo id_persona desde la bdd con idPersona dentro de esta clase 
     private int idPersona;
-    
+
     private String nombre;
-    
+
     private String apellido;
-    
+
     private String email;
-    
+
     private String telefono;
 
     /* Contructores */
@@ -80,12 +79,11 @@ public class Persona implements Serializable{
 
     @Override
     public String toString() {
-        return "Persona: " + "idPersona=" + idPersona 
-                           + ", nombre=" + nombre
-                           + ", apellido=" + apellido 
-                           + ", email=" + email 
-                           + ", telefono=" + telefono;
+        return "Persona: " + "idPersona=" + idPersona
+                + ", nombre=" + nombre
+                + ", apellido=" + apellido
+                + ", email=" + email
+                + ", telefono=" + telefono;
     }
-    
-    
+
 }
