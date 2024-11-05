1c59fff1daa3b535014ce145f04c24db75acba8d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-04 23:30:53 -0500

    Mediante la ayuda del IDE se mapea la entidad persona desde la base de datos y se crea la clase persona con todos los atributos, anotaciones que necesita una clase para transformarse en una entidad en java

--- EJB/sga-jee-web-v2/src/main/java/ec/com/ib/sga/domain/Persona.java
+++ EJB/sga-jee-web-v2/src/main/java/ec/com/ib/sga/domain/Persona.java
@@ -1,57 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ec.com.ib.sga.domain;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
 
-/*
-    Esta clase se va crear como una ENTIDAD para poder interactuar con el api de JPA hacia la base de datos y esto se hace
-    mediante la notación @Entity
+/**
+ *
+ * @author Bryan
  */
 @Entity
-                    // query para obtener todos los datos de la base de datos como objetos
 @NamedQueries({
-    @NamedQuery(name="Persona.findAll", query="SELECT p FROM Persona p ORDER BY p.idPersona")
-})
-@Table(name="persona")  // en caso de que la entidad en la base tenga otro nombre se la define así
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p"),
+    @NamedQuery(name = "Persona.findByIdPersona", query = "SELECT p FROM Persona p WHERE p.idPersona = :idPersona"),
+    @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre"),
+    @NamedQuery(name = "Persona.findByApellido", query = "SELECT p FROM Persona p WHERE p.apellido = :apellido"),
+    @NamedQuery(name = "Persona.findByEmail", query = "SELECT p FROM Persona p WHERE p.email = :email"),
+    @NamedQuery(name = "Persona.findByTelefono", query = "SELECT p FROM Persona p WHERE p.telefono = :telefono")})
 public class Persona implements Serializable {
 
-    private static final long serialVersionUID = 1L;  // esta variable es una implementación necesaria que viene de la interface Serializable
-
-    @Id                                                 // Anotacion que indetifica a idPersona como la clave primaría
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Anotación que dice que es un atributo autoincremental dentro de la base de datos
-    @Column(name = "id_persona")                         // mapeo del atributo id_persona desde la bdd con idPersona dentro de esta clase 
-    private int idPersona;
-
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_persona")
+    private Integer idPersona;
+    @Size(max = 45)
     private String nombre;
-
+    @Size(max = 45)
     private String apellido;
-
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 55)
     private String email;
-
+    @Size(max = 10)
     private String telefono;
+    @OneToMany(mappedBy = "persona")
+    private List<Usuario> usuarioList;
 
-    /* Contructores */
     public Persona() {
     }
 
-    public Persona(String nombre, String apellido, String email, String telefono) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.telefono = telefono;
+    public Persona(Integer idPersona) {
+        this.idPersona = idPersona;
     }
 
-    public int getIdPersona() {
-        return this.idPersona;
+    public Integer getIdPersona() {
+        return idPersona;
     }
 
-    public void setIdPersona(int idPersona) {
+    public void setIdPersona(Integer idPersona) {
         this.idPersona = idPersona;
     }
 
     public String getNombre() {
-        return this.nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
@@ -59,7 +73,7 @@ public class Persona implements Serializable {
     }
 
     public String getApellido() {
-        return this.apellido;
+        return apellido;
     }
 
     public void setApellido(String apellido) {
@@ -67,7 +81,7 @@ public class Persona implements Serializable {
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
 
     public void setEmail(String email) {
@@ -75,20 +89,44 @@ public class Persona implements Serializable {
     }
 
     public String getTelefono() {
-        return this.telefono;
+        return telefono;
     }
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    @Override
-    public String toString() {
-        return "Persona: " + "idPersona=" + idPersona
-                + ", nombre=" + nombre
-                + ", apellido=" + apellido
-                + ", email=" + email
-                + ", telefono=" + telefono;
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
     }
 
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPersona != null ? idPersona.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.idPersona == null && other.idPersona != null) || (this.idPersona != null && !this.idPersona.equals(other.idPersona))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ec.com.ib.sga.domain.Persona[ idPersona=" + idPersona + " ]";
+    }
+    
 }
