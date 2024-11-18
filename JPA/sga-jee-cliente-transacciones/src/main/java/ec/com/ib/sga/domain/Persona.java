89b8bcf8047298edc295297a43b256cb14d38fbb
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-17 21:59:00 -0500

    Cambiando la configuración del archivo persistence para que funcione con un tipo de transacciones JTA

--- JPA/sga-jee-cliente-transacciones/src/main/java/ec/com/ib/sga/domain/Persona.java
+++ JPA/sga-jee-cliente-transacciones/src/main/java/ec/com/ib/sga/domain/Persona.java
@@ -1,42 +1,64 @@
 package ec.com.ib.sga.domain;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 
-/*
-    SEGUNDO PASO: Se crea la clase Persona, para enviarse del servidor al cliente esta clase necesita serializarse mediante su implementación a esta clase
-*/
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p"),
+    @NamedQuery(name = "Persona.findByIdPersona", query = "SELECT p FROM Persona p WHERE p.idPersona = :idPersona"),
+    @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre"),
+    @NamedQuery(name = "Persona.findByApellido", query = "SELECT p FROM Persona p WHERE p.apellido = :apellido"),
+    @NamedQuery(name = "Persona.findByEmail", query = "SELECT p FROM Persona p WHERE p.email = :email"),
+    @NamedQuery(name = "Persona.findByTelefono", query = "SELECT p FROM Persona p WHERE p.telefono = :telefono")})
+public class Persona implements Serializable {
 
-public class Persona implements Serializable{
+    private static final long serialVersionUID = 1L;
     
-    private static final long serialVersionUID = 1L;        // atributo requerido por la interfaz Serializable
-    
-    private int idPersona;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_persona")
+    private Integer idPersona;
+    @Size(max = 45)
     private String nombre;
+    @Size(max = 45)
     private String apellido;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 45)
     private String email;
+    @Size(max = 45)
     private String telefono;
+    
+    @OneToMany(mappedBy = "persona", cascade = CascadeType.ALL)
+    private List<Usuario> usuarioList;
 
     public Persona() {
     }
 
-    public Persona(int idPersona, String nombre, String apellido, String email, String telefono) {
-        this.idPersona = idPersona;
+    public Persona(String nombre, String apellido, String email, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.telefono = telefono;
     }
-
-    public int getIdPersona() {
-        return this.idPersona;
+    
+    public Persona(Integer idPersona) {
+        this.idPersona = idPersona;
     }
 
-    public void setIdPersona(int idPersona) {
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
         this.idPersona = idPersona;
     }
 
     public String getNombre() {
-        return this.nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
@@ -44,7 +66,7 @@ public class Persona implements Serializable{
     }
 
     public String getApellido() {
-        return this.apellido;
+        return apellido;
     }
 
     public void setApellido(String apellido) {
@@ -52,7 +74,7 @@ public class Persona implements Serializable{
     }
 
     public String getEmail() {
-        return this.email;
+        return email;
     }
 
     public void setEmail(String email) {
@@ -60,11 +82,39 @@ public class Persona implements Serializable{
     }
 
     public String getTelefono() {
-        return this.telefono;
+        return telefono;
     }
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPersona != null ? idPersona.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.idPersona == null && other.idPersona != null) || (this.idPersona != null && !this.idPersona.equals(other.idPersona))) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -73,8 +123,7 @@ public class Persona implements Serializable{
                            + ", nombre=" + nombre 
                            + ", apellido=" + apellido 
                            + ", email=" + email 
-                           + ", telefono=" + telefono;
+                           + ", telefono=" + telefono + '}';
     }
-    
-    
+
 }
