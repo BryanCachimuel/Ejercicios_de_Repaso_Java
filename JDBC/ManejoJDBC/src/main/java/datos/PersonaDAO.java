e9e019dd676313e1e97803b3012f9a49895396b6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-18 21:18:15 -0500

    Directorio con los ejercicios en donde se aplica el uso de JDBC

---
+++ JDBC/ManejoJDBC/src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,131 @@
+package datos;
+
+import domain.Persona;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PersonaDAO {
+
+    private static final String SQL_SELECT = "SELECT id_persona, nombre,apellido, email, telefono FROM persona";
+    private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET nombre=?, apellido=?, email=?, telefono=? WHERE id_persona=?";
+    private static final String SQL_DELETE = "DELETE FROM persona WHERE id_persona = ?";
+
+    public List<Persona> seleccionar() {
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        ResultSet rs = null;
+        Persona persona;
+        List<Persona> personas = new ArrayList<>();
+
+        try {
+            conn = Conexion.getConnection();
+            psmtm = conn.prepareStatement(SQL_SELECT);
+            rs = psmtm.executeQuery();
+            while (rs.next()) {
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+
+                // se usa el constructor con toda la información para agregar una persona
+                persona = new Persona(idPersona, nombre, apellido, email, telefono);
+                personas.add(persona);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(psmtm);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return personas;
+    }
+
+    public int insertar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        int registros = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            psmtm = conn.prepareStatement(SQL_INSERT);
+            psmtm.setString(1, persona.getNombre());
+            psmtm.setString(2, persona.getApellido());
+            psmtm.setString(3, persona.getEmail());
+            psmtm.setString(4, persona.getTelefono());
+            // se hace este procedimiento por que estamos alterando la base de datos y un executeUpdate puede facilitar el procedimiento de inserción
+            registros = psmtm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(psmtm);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+
+        }
+        return registros;
+    }
+
+    public int actualizar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        int registroActualizado = 0;
+        
+        try {
+            conn = Conexion.getConnection();
+            psmtm = conn.prepareStatement(SQL_UPDATE);
+            psmtm.setString(1, persona.getNombre());
+            psmtm.setString(2, persona.getApellido());
+            psmtm.setString(3, persona.getEmail());
+            psmtm.setString(4, persona.getTelefono());
+            psmtm.setInt(5, persona.getIdPersona());
+            registroActualizado = psmtm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            try {
+                Conexion.close(conn);
+                Conexion.close(psmtm);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        
+        return registroActualizado;
+    }
+    
+    public int eliminar(Persona persona){
+        Connection conn = null;
+        PreparedStatement psmtm = null;
+        int registroEliminar = 0;
+        
+        try {
+            conn = Conexion.getConnection();
+            psmtm = conn.prepareStatement(SQL_DELETE);
+            psmtm.setInt(1, persona.getIdPersona());
+            registroEliminar = psmtm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            try {
+                Conexion.close(conn);
+                Conexion.close(psmtm);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registroEliminar;
+    }
+}
---
+++ JDBC/ManejoJDBC/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,40 @@
+package test;
+
+import datos.PersonaDAO;
+import domain.Persona;
+import java.util.List;
+
+public class TestManejoPersonas {
+    
+    public static void main(String[] args) {
+        PersonaDAO personaDao = new PersonaDAO();
+        
+        
+        /* se puede usar los dos procesos
+         personas.forEach(persona -> {
+            System.out.println("Persona: " + persona);
+        });
+        for(Persona persona : personas){
+            System.out.println("Persona: " + persona);
+        }*/
+        
+        /*Insertando un nuevo objeto de tipo persona 
+        Persona personaNueva = new Persona("Maritza", "Paspuezan", "mp@yahoo.com", "0945793652");
+        personaDao.insertar(personaNueva);*/
+        
+        /* Modificar un objeto de persona ya existente 
+        Persona personaActualizar = new Persona(4, "Kevin", "Rodriguez", "kdrb@outlook.com", "0984526973");
+        personaDao.actualizar(personaActualizar);*/
+        
+        /* eliminar un registro */
+        Persona personaEliminada = new Persona(3);
+        personaDao.eliminar(personaEliminada);
+        
+        List<Persona> personas = personaDao.seleccionar();
+        /* es recomendable usar una función lamda */
+        personas.forEach(persona -> {
+            System.out.println("Persona: " + persona);
+        });
+    }
+    
+}
