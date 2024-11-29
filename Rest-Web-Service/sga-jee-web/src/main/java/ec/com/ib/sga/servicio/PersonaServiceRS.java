3414468a188a07a71d13080a30410ce26094a7bf
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-28 21:37:04 -0500

    Creando la clase PersonaServiceRS el cual contiene los métodos crud pero invocados con los decoradores @GET y @Produces para los métodos listar y encontrar persona por id

---
+++ Rest-Web-Service/sga-jee-web/src/main/java/ec/com/ib/sga/servicio/PersonaServiceRS.java
@@ -0,0 +1,33 @@
+package ec.com.ib.sga.servicio;
+
+import ec.com.ib.sga.domain.Persona;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("/personas")
+@Stateless
+public class PersonaServiceRS {
+    
+    @Inject
+    private PersonaService personaService;
+    
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Persona> listarPersonas(){
+        return personaService.listarPersonas();
+    }
+    
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Path("{id}")       // hace referencia a /personas/{id}
+    public Persona encontrarPersonaPorId(@PathParam("id") int id){
+        return personaService.encontrarPersonaPorId(new Persona(id));
+    }
+    
+}
+
+
