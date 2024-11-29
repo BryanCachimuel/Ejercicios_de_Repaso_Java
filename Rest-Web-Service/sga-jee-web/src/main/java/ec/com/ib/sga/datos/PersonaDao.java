61a60382efc0a3b787e7fe970b10866a85c73c06
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-28 21:10:33 -0500

    Restableciendo archivos y configuraciones iniciales del proyecto

---
+++ Rest-Web-Service/sga-jee-web/src/main/java/ec/com/ib/sga/datos/PersonaDao.java
@@ -0,0 +1,19 @@
+package ec.com.ib.sga.datos;
+
+import java.util.List;
+import ec.com.ib.sga.domain.Persona;
+
+public interface PersonaDao {
+    public List<Persona> findAllPersonas();
+    
+    public Persona findPersonaById(Persona persona);
+    
+    public Persona findPersonaByEmail(Persona persona);
+    
+    public void insertPersona(Persona persona);
+    
+    public void updatePersona(Persona persona);
+    
+    public void deletePersona(Persona persona);
+    
+}
