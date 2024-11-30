650df6c93c3a0682863d14c916f9c6182e0d23f6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-29 21:30:09 -0500

    Realizamos las consultas para buscar un persona por id y listar todas las personas registradas

--- Rest-Web-Service/ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
+++ Rest-Web-Service/ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
@@ -15,4 +15,28 @@ public class TestPersonaServiceRS {
     private static List<Persona> personas;
     private static Invocation.Builder invocationBuilder;
     private static Response response;
+    
+    public static void main(String[] args) {
+        cliente = ClientBuilder.newClient();
+        
+        //Leer una persona (método get) 
+        webTarget = cliente.target(URL_BASE).path("/personas");
+        
+        // Proporcionamos un idPersona valido
+        persona = webTarget.path("/1").request(MediaType.APPLICATION_XML).get(Persona.class);
+        System.out.println("Persona recuperada: " + persona);
+        
+        System.out.println("\n");
+        
+        // Leer todas las personas (método get con readEntity de tipo List<>)
+        personas = webTarget.request(MediaType.APPLICATION_XML).get(Response.class).readEntity(new GenericType<List<Persona>>(){});
+        System.out.println("Personas Recuperadas");
+        imprimirPersonas(personas);
+    }
+
+    private static void imprimirPersonas(List<Persona> personas) {
+        for(Persona p : personas){
+            System.out.println("Personas:" + p);
+        }
+    }
 }
