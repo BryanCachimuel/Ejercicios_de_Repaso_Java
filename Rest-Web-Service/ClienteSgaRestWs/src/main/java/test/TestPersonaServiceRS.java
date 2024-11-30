3e06f26f06be3146bedd8217e007334bdc71de45
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-11-29 21:15:07 -0500

    Creando la clase TestPersonaServiceRS en la cual por el momento se definieron las variables que se van a utilizar para lograr consumir los endpoints del backend hacia el cliente

---
+++ Rest-Web-Service/ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
@@ -0,0 +1,18 @@
+package test;
+
+import domain.Persona;
+import java.util.List;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
+
+public class TestPersonaServiceRS {
+    
+    /* Variables que vamos a utilizar */
+    private static final String URL_BASE = "http://localhost:8095/sga-jee-web/webservice";
+    private static Client cliente;
+    private static WebTarget webTarget;
+    private static Persona persona;
+    private static List<Persona> personas;
+    private static Invocation.Builder invocationBuilder;
+    private static Response response;
+}
