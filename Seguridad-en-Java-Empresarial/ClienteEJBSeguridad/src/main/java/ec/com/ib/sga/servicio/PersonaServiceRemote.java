9ee145d28a4bd1fa82b7c2b4f42e3de8ff7721c1
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-02 21:07:05 -0500

    Creando una interfaz con el nombre PersonaServiceRemote la cual contiene todos los métodos crud que va a tener el proyecto

---
+++ Seguridad-en-Java-Empresarial/ClienteEJBSeguridad/src/main/java/ec/com/ib/sga/servicio/PersonaServiceRemote.java
@@ -0,0 +1,20 @@
+package ec.com.ib.sga.servicio;
+
+import ec.com.ib.sga.domain.Persona;
+import java.util.List;
+
+public interface PersonaServiceRemote {
+    
+    public List<Persona> listarPersonas();
+    
+    public Persona encontrarPersonaPorId(Persona persona);
+    
+    public Persona encontrarPersonaPorEmail(Persona persona);
+    
+    public void registrarPersona(Persona persona);
+    
+    public void modificarPersona(Persona persona);
+    
+    public void eliminarPersona(Persona persona);
+    
+}
