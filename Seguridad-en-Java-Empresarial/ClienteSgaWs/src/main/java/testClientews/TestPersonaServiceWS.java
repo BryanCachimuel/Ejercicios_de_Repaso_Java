1ea2b7f297cee44428c33d2e2678fb1a7c8fa394
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-03 22:26:26 -0500

    Integrando las seguridades para acceder a la prueba de funcionamiento del service de personas

--- Seguridad-en-Java-Empresarial/ClienteSgaWs/src/main/java/testClientews/TestPersonaServiceWS.java
+++ Seguridad-en-Java-Empresarial/ClienteSgaWs/src/main/java/testClientews/TestPersonaServiceWS.java
@@ -4,12 +4,16 @@ import clientews.servicio.Persona;
 import clientews.servicio.PersonaServiceImplService;
 import clientews.servicio.PersonaServiceWs;
 import java.util.List;
+import javax.xml.ws.BindingProvider;
 
 public class TestPersonaServiceWS {
     
     public static void main(String[] args) {
         
         PersonaServiceWs personaService = new PersonaServiceImplService().getPersonaServiceImplPort();
+        
+        ((BindingProvider)personaService).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "admin");
+        ((BindingProvider)personaService).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "admin");
         
         System.out.println("Ejecutando Servicio listarPersonas ws");
         List<Persona> personas = personaService.listarPersonas();
