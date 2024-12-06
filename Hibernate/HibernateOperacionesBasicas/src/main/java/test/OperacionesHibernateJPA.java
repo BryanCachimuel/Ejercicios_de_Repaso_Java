8862854a1a8d576be037c1e8cfcf72669c7b79cc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-05 21:37:01 -0500

    Realizando un test del procedimiento de inserción de una nueva persona

--- Hibernate/HibernateOperacionesBasicas/src/main/java/test/OperacionesHibernateJPA.java
+++ Hibernate/HibernateOperacionesBasicas/src/main/java/test/OperacionesHibernateJPA.java
@@ -1,12 +1,22 @@
 package test;
 
 import ec.com.ib.dao.PersonaDAO;
+import ec.com.ib.domain.Persona;
 
 
 public class OperacionesHibernateJPA {
     
     public static void main(String[] args) {
         PersonaDAO personaDao = new PersonaDAO();
+        //personaDao.listar();
+        
+        Persona persona = new Persona();
+        persona.setNombre("Maritza");
+        persona.setApellido("Paspuezan");
+        persona.setEmail("mp@gmail.com");
+        persona.setTelefono("0985741236");
+        
+        //personaDao.insertar(persona);
         personaDao.listar();
     }
     
