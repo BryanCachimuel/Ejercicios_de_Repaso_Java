34abcca3f31abc8d1623324b450f374dac0e056d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-05 21:58:03 -0500

    Realizando un test de los métodos modificar y buscarPersonaPorId para poder realizar una modificación de los datos de un registro

--- Hibernate/HibernateOperacionesBasicas/src/main/java/test/OperacionesHibernateJPA.java
+++ Hibernate/HibernateOperacionesBasicas/src/main/java/test/OperacionesHibernateJPA.java
@@ -8,15 +8,25 @@ public class OperacionesHibernateJPA {
     
     public static void main(String[] args) {
         PersonaDAO personaDao = new PersonaDAO();
-        //personaDao.listar();
         
+        /* Para realizar un registro */
         Persona persona = new Persona();
         persona.setNombre("Maritza");
         persona.setApellido("Paspuezan");
         persona.setEmail("mp@gmail.com");
         persona.setTelefono("0985741236");
-        
         //personaDao.insertar(persona);
+        
+        /* Para actualizar un registro */
+        Persona personaModificada = new Persona();
+        personaModificada.setIdPersona(17);
+        
+        personaModificada = personaDao.buscarPersonaPorId(personaModificada);
+        
+        personaModificada.setNombre("Richard");
+        personaModificada.setEmail("rftc@outlook.com");
+        personaDao.modificar(personaModificada);
+        
         personaDao.listar();
     }
     
