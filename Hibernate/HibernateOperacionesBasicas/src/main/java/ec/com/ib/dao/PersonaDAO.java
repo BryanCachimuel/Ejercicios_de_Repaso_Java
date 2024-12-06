c7f8d5f690e4c6f827bcc068a5636a31e688ac32
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-05 21:24:30 -0500

    Utilizando el patron de diseño DAO para la entidad Persona en la cual se ha creado el método listar para obtener todos los registros de la base de datos

---
+++ Hibernate/HibernateOperacionesBasicas/src/main/java/ec/com/ib/dao/PersonaDAO.java
@@ -0,0 +1,27 @@
+package ec.com.ib.dao;
+
+import ec.com.ib.domain.Persona;
+import jakarta.persistence.*;
+import java.util.List;
+
+public class PersonaDAO {
+    
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public PersonaDAO() {
+        // se manda a llamar a la unidad de persistencia establecida en el archivo persistence.xml
+        emf = Persistence.createEntityManagerFactory("HibernatePU");
+        em = emf.createEntityManager();
+    }
+    
+    public void listar(){
+        String hql = "SELECT p FROM Persona p";
+        Query query = em.createQuery(hql);
+        List<Persona> personas = query.getResultList();
+        for(Persona p : personas){
+            System.out.println("personas: " + p);
+        }
+    }
+    
+}
