35a8657f34f4c1d0a206f6bfd932827ca89696df
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-09 20:07:15 -0500

    Creando el proyecto SgaWebHibernateJPA para aprender el uso de asociaciones

---
+++ Hibernate/SgaWebHibernateJPA/src/main/java/ec/com/ib/SgaWebHibernateJPA/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package ec.com.ib.SgaWebHibernateJPA;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
