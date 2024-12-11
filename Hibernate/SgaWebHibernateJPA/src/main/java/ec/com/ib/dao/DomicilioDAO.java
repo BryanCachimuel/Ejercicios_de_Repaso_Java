99185876f8b965b46943ec1d9a652a1d7e686b89
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-10 20:01:30 -0500

    Creando la clase DomicilioDAO donde se definen los métodos CRUD para la entidad domicilio de la base de datos

---
+++ Hibernate/SgaWebHibernateJPA/src/main/java/ec/com/ib/dao/DomicilioDAO.java
@@ -0,0 +1,68 @@
+package ec.com.ib.dao;
+
+import ec.com.ib.domain.Domicilio;
+import java.util.List;
+import javax.persistence.Query;
+
+public class DomicilioDAO extends GenericDAO{
+    
+    public List<Domicilio> listar(){
+        String consulta = "SELECT d FROM Domicilio d";
+        em = getEntityManager();
+        Query query = em.createNamedQuery(consulta);
+        return query.getResultList();
+    }
+    
+    public void insertar(Domicilio domicilio){
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(domicilio);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            if(em != null){
+                em.close();
+            }
+        }
+    }
+    
+    public void actualizar(Domicilio domicilio){
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(domicilio);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            if(em != null){
+                em.close();
+            }
+        }
+    }
+    
+    public void eliminar(Domicilio domicilio){
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.merge(domicilio));
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            if(em != null){
+                em.close();
+            }
+        }
+    }
+    
+    public Object buscarPoId(Domicilio domicilio){
+        em = getEntityManager();
+        return em.find(Domicilio.class, domicilio.getIdDomicilio());
+    }
+}
