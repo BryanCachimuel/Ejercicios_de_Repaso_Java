0edd65a635cf701db619567b0f634841f3335046
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-11 22:32:51 -0500

    Preparando el proyecto SgaWebHibernateJPA para el proceso del ciclo de vida de Hibernate

---
+++ Hibernate/SgaWebHibernateCicloVidaJPA/src/main/java/ec/com/ib/dao/AlumnoDAO.java
@@ -0,0 +1,67 @@
+package ec.com.ib.dao;
+
+import static ec.com.ib.dao.GenericDAO.em;
+import ec.com.ib.domain.Alumno;
+import java.util.List;
+import javax.persistence.Query;
+
+public class AlumnoDAO extends GenericDAO {
+
+    public List<Alumno> listar() {
+        String consulta = "SELECT a FROM Alumno a";
+        em = getEntityManager();
+        Query query = em.createQuery(consulta);
+        return query.getResultList();
+    }
+
+    public void insertar(Alumno alumno) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(alumno);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void actualizar(Alumno alumno) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(alumno);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void eliminar(Alumno alumno) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.merge(alumno));
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Object buscarPorId(Alumno alumno) {
+        em = getEntityManager();
+        return em.find(Alumno.class, alumno.getIdAlumno());
+    }
+
+}
---
+++ Hibernate/SgaWebHibernateCicloVidaJPA/src/main/java/ec/com/ib/test/TestDAOs.java
@@ -0,0 +1,43 @@
+package ec.com.ib.test;
+
+import ec.com.ib.dao.AlumnoDAO;
+import ec.com.ib.dao.AsignacionDAO;
+import ec.com.ib.dao.ContactoDAO;
+import ec.com.ib.dao.CursoDAO;
+import ec.com.ib.dao.DomicilioDAO;
+import java.util.List;
+
+public class TestDAOs {
+    
+    public static void main(String[] args) {
+        
+        /*AlumnoDAO alumnoDao = new AlumnoDAO();
+        System.out.println("Alumnos: ");
+        imprimir(alumnoDao.listar());*/
+        
+        /*DomicilioDAO domicilioDao = new DomicilioDAO();
+        System.out.println("Domicilio: ");
+        imprimir(domicilioDao.listar());*/
+        
+        /*CursoDAO cursoDao = new CursoDAO();
+        System.out.println("Curso: ");
+        imprimir(cursoDao.listar());*/
+        
+        ContactoDAO contactoDao = new ContactoDAO();
+        System.out.println("Contacto: ");
+        imprimir(contactoDao.listar());
+        
+        /*AsignacionDAO asignacionDao = new AsignacionDAO();
+        System.out.println("Contacto: ");
+        imprimir(asignacionDao.listar());*/
+        
+        
+    }
+    
+    private static void imprimir(List coleccion){
+        for(Object o : coleccion){
+            System.out.println("Valor: " + o);
+        }
+    }
+    
+}
