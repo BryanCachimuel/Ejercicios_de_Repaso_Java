b623ebac5c4fbb6ca7f322eaaa4bce69d4b57700
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-13 21:21:44 -0500

    Realizando la prueba de modificar un objeto persistente en el ciclo de vida de hibernate

---
+++ Hibernate/SgaWebHibernateCicloVidaJPA/src/main/java/ec/com/ib/test/ciclovida/Estado3ModificarObjetoPersistente.java
@@ -0,0 +1,38 @@
+package ec.com.ib.test.ciclovida;
+
+import ec.com.ib.domain.Contacto;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Estado3ModificarObjetoPersistente {
+    
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateJpaPU");
+        EntityManager em = emf.createEntityManager();
+        
+        // Se define la variable
+        Contacto contacto = null;
+        
+         // recuperamos el objeto
+         // 1. Transitivo
+        contacto = em.find(Contacto.class, 3);
+        
+        // Modificamos el objeto
+        contacto.setNombre("Lene Otavalo");
+        contacto.setTelefono("0984129876");
+        contacto.setEmail("lo@outlook.com");
+        
+        em.getTransaction().begin();
+        
+         // 2. persistente
+        em.persist(contacto);
+    
+        em.getTransaction().commit();
+        
+        // detached 
+        System.out.println("contacto = " + contacto);
+    }
+    
+}
