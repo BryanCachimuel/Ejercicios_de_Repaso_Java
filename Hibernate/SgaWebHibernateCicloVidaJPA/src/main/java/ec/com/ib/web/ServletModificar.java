a58e07c7dd039bf6444d62fb053a53ff3284d0ae
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-16 19:01:54 -0500

    Agregando el método doPost el cual se encargará de realizar las actualizaciones de cualquier dato del alumno que se seleccione

--- Hibernate/SgaWebHibernateCicloVidaJPA/src/main/java/ec/com/ib/web/ServletModificar.java
+++ Hibernate/SgaWebHibernateCicloVidaJPA/src/main/java/ec/com/ib/web/ServletModificar.java
@@ -1,6 +1,8 @@
 package ec.com.ib.web;
 
 import ec.com.ib.domain.Alumno;
+import ec.com.ib.domain.Contacto;
+import ec.com.ib.domain.Domicilio;
 import ec.com.ib.servicio.ServicioAlumno;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,23 +10,61 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/ServletModificar")
-public class ServletModificar extends HttpServlet{
+public class ServletModificar extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // recuperar el id que se envia desde el jsp de listarAlumnos
         String idAlumnoS = request.getParameter("idAlumno");
         Integer idAlumno = Integer.parseInt(idAlumnoS);
-        
+
         Alumno alumno = new Alumno();
         alumno.setIdAlumno(idAlumno);
-        
+
         ServicioAlumno servicioAlumno = new ServicioAlumno();
         alumno = servicioAlumno.encontrarAlumno(alumno);        // se almacena en una variable alumno para poder recuperar toda la información de ese alumno
-        
-        request.setAttribute("alumno", alumno);
+
+        //request.setAttribute("alumno", alumno);
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("alumno", alumno);
+
         request.getRequestDispatcher("/WEB-INF/modificarAlumno.jsp").forward(request, response);
     }
-    
-    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        Integer edad = Integer.parseInt(request.getParameter("edad"));
+
+        String calle = request.getParameter("calle");
+        String noCasa = request.getParameter("noCasa");
+        String ciudad = request.getParameter("ciudad");
+
+        String nombreContacto = request.getParameter("nombreContacto");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+
+        HttpSession sesion = request.getSession();
+        Alumno alumno = (Alumno) sesion.getAttribute("alumno");   // se obtiene todos los datos de alumno mediante la sesión iniciada
+
+        alumno.setNombre(nombre);
+        alumno.setApellido(apellido);
+        alumno.setEdad(edad);
+        alumno.getDomicilio().setCalle(calle);
+        alumno.getDomicilio().setNoCasa(noCasa);
+        alumno.getDomicilio().setCiudad(ciudad);
+        alumno.getContacto().setNombreContacto(nombreContacto);
+        alumno.getContacto().setEmail(email);
+        alumno.getContacto().setTelefono(telefono);
+
+        ServicioAlumno servicioAlumno = new ServicioAlumno();
+        servicioAlumno.guardarAlumno(alumno);
+        
+        sesion.removeAttribute("alumno");
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+
 }
