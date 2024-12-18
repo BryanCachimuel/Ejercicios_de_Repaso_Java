ab6e3bed89d9a8af628c7d98bc7fefa5dfc65b33
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-17 20:47:46 -0500

    Creando un proyecto para iniciar con el proceso de aprendizaje del framework spring

---
+++ Spring/IntroduccionSpring/src/main/java/ec/com/ib/IntroduccionSpringApplication.java
@@ -0,0 +1,13 @@
+package ec.com.ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduccionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroduccionSpringApplication.class, args);
+	}
+
+}
